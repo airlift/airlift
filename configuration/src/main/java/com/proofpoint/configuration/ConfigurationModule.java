b4ab142395996976d47184dbc4ae9f6d235d1960
Martin Traverso <mtraverso@acm.org>
2010-09-07 18:10:02 -0700

    add TODO

--- configuration/src/main/java/com/proofpoint/configuration/ConfigurationModule.java
+++ configuration/src/main/java/com/proofpoint/configuration/ConfigurationModule.java
@@ -16,6 +16,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * TODO: add mechanism for dumping current config
+ */
 public class ConfigurationModule
         implements Module
 {
