d97a3575bccf176105dc9a9dbab830e8e5d08398
Martin Traverso <mtraverso@acm.org>
2010-09-14 09:12:26 -0700

    add tests for variations with Throwable

--- log/src/test/java/com/proofpoint/log/TestLogger.java
+++ log/src/test/java/com/proofpoint/log/TestLogger.java
@@ -9,9 +9,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertTrue;
 
-/**
- * TODO: test Throwable variations
- */
 public class TestLogger
 {
     @Test
@@ -55,10 +52,18 @@ public class TestLogger
         org.slf4j.Logger mockLogger = mock(org.slf4j.Logger.class);
         when(mockLogger.isWarnEnabled()).thenReturn(true);
 
+        // message-only version
         Logger logger = new Logger(mockLogger);
         logger.warn("hello, %s", "you");
 
         verify(mockLogger).warn("hello, you");
+
+        // throwable with message
+        @SuppressWarnings({ "ThrowableInstanceNeverThrown" })
+        Throwable exception = new Throwable();
+
+        logger.warn(exception, "got exception: %s", "foo");
+        verify(mockLogger).warn("got exception: foo", exception);
     }
 
     @Test
@@ -67,10 +72,24 @@ public class TestLogger
         org.slf4j.Logger mockLogger = mock(org.slf4j.Logger.class);
         when(mockLogger.isErrorEnabled()).thenReturn(true);
 
+        // message-only version
         Logger logger = new Logger(mockLogger);
         logger.error("hello, %s", "you");
 
         verify(mockLogger).error("hello, you");
+
+        // throwable with message
+        @SuppressWarnings({ "ThrowableInstanceNeverThrown" })
+        Throwable exception = new Throwable();
+
+        logger.error(exception, "got exception: %s", "foo");
+        verify(mockLogger).error("got exception: foo", exception);
+
+        // throwable alone
+        @SuppressWarnings({ "ThrowableInstanceNeverThrown" })
+        Throwable exception2 = new Throwable("the message");
+        logger.error(exception2);
+        verify(mockLogger).error(exception2.getMessage(), exception2);
     }
 
 
@@ -114,6 +133,22 @@ public class TestLogger
     }
 
     @Test
+    public void testWarnWithThrowableShortCircuit()
+    {
+        org.slf4j.Logger mockLogger = mock(org.slf4j.Logger.class);
+
+        when(mockLogger.isWarnEnabled()).thenReturn(false);
+
+        Logger logger = new Logger(mockLogger);
+
+        @SuppressWarnings({ "ThrowableInstanceNeverThrown" })
+        Throwable e = new Throwable();
+        logger.warn(e, "hello");
+
+        verifyNoCalls(mockLogger);
+    }
+
+    @Test
     public void testErrorShortCircuit()
     {
         org.slf4j.Logger mockLogger = mock(org.slf4j.Logger.class);
@@ -125,6 +160,39 @@ public class TestLogger
 
         verifyNoCalls(mockLogger);
     }
+
+    @Test
+    public void testErrorWithThrowableShortCircuit()
+    {
+        org.slf4j.Logger mockLogger = mock(org.slf4j.Logger.class);
+
+        when(mockLogger.isErrorEnabled()).thenReturn(false);
+
+        Logger logger = new Logger(mockLogger);
+
+        @SuppressWarnings({ "ThrowableInstanceNeverThrown" })
+        Throwable e = new Throwable();
+        logger.error(e, "hello");
+
+        verifyNoCalls(mockLogger);
+    }
+
+    @Test
+    public void testErrorWithThrowableNoMessageShortCircuit()
+    {
+        org.slf4j.Logger mockLogger = mock(org.slf4j.Logger.class);
+
+        when(mockLogger.isErrorEnabled()).thenReturn(false);
+
+        Logger logger = new Logger(mockLogger);
+
+        @SuppressWarnings({ "ThrowableInstanceNeverThrown" })
+        Throwable e = new Throwable();
+        logger.error(e);
+
+        verifyNoCalls(mockLogger);
+    }
+
 
     private void verifyNoCalls(org.slf4j.Logger mockLogger)
     {
@@ -152,4 +220,5 @@ public class TestLogger
         verify(mockLogger, never()).error(any(String.class), any(Object[].class));
         verify(mockLogger, never()).error(any(String.class), any(Throwable.class));
     }
+
 }
