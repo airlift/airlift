fc481621ea65b5a347dc5c294dfda040bc38cf28
Jordan Zimmerman <jordan@proofpoint.com>
2010-10-13 17:14:10 -0700

    Make sure JMXAgent gets bound

--- jmx/src/main/java/com/proofpoint/jmx/JMXModule.java
+++ jmx/src/main/java/com/proofpoint/jmx/JMXModule.java
@@ -2,6 +2,7 @@ package com.proofpoint.jmx;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
+import com.google.inject.Scopes;
 import com.proofpoint.configuration.ConfigurationModule;
 
 import javax.management.MBeanServer;
@@ -10,9 +11,11 @@ import java.lang.management.ManagementFactory;
 public class JMXModule
         implements Module
 {
+    @Override
     public void configure(Binder binder)
     {
         binder.bind(MBeanServer.class).toInstance(ManagementFactory.getPlatformMBeanServer());
+        binder.bind(JMXAgent.class).in(Scopes.SINGLETON);
         ConfigurationModule.bindConfig(binder, JMXConfig.class);
     }
 }
