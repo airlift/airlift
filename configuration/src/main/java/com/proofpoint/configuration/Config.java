cbe66c44d1ac8b700d2cb8c21a650d07a04da7c4
Dain Sundstrom <dain@iq80.com>
2010-12-07 10:29:25 -0800

    @Config is now @Documented

--- configuration/src/main/java/com/proofpoint/configuration/Config.java
+++ configuration/src/main/java/com/proofpoint/configuration/Config.java
@@ -6,7 +6,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Documented;
 
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @BindingAnnotation
