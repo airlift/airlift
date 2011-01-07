b9ace0675bd2ac4597ede9f0dd9bf82b97a42766
Martin Traverso <mtraverso@acm.org>
2011-01-07 13:47:41 -0800

    introduce setup & teardown methods and move file creation & deletion logic

--- dbpool/src/test/java/com/proofpoint/dbpool/H2EmbeddedDataSourceTest.java
+++ dbpool/src/test/java/com/proofpoint/dbpool/H2EmbeddedDataSourceTest.java
@@ -1,31 +1,43 @@
 package com.proofpoint.dbpool;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import com.proofpoint.dbpool.H2EmbeddedDataSourceConfig.Cipher;
 
 public class H2EmbeddedDataSourceTest
 {
+    private File file;
+
+    @BeforeMethod
+    public void setup()
+            throws IOException
+    {
+        file = File.createTempFile("h2db-", ".db");
+    }
+
+    @AfterMethod
+    public void teardown()
+    {
+        file.delete();
+    }
+
     @Test
     public void test()
             throws Exception
     {
-        String fileName = File.createTempFile("h2db-", ".db").getAbsolutePath();
-
         H2EmbeddedDataSourceConfig config = new H2EmbeddedDataSourceConfig()
-                .setFilename(fileName)
+                .setFilename(file.getAbsolutePath())
                 .setInitScript("com/proofpoint/dbpool/h2.ddl")
                 .setCipher(Cipher.AES)
                 .setFilePassword("filePassword");
 
-        try {
-            H2EmbeddedDataSource dataSource = new H2EmbeddedDataSource(config);
-            dataSource.getConnection().createStatement().executeQuery("select * from message");
-        }
-        finally {
-            new File(config.getFilename()).delete();
-        }
+        H2EmbeddedDataSource dataSource = new H2EmbeddedDataSource(config);
+        dataSource.getConnection().createStatement().executeQuery("select * from message");
     }
 }
