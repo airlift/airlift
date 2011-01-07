36758dd8115ae7c8ae12bd9c957a2d47b5b0206e
Mark Kent <mkent@proofpoint.com>
2011-01-07 09:51:53 -0800

    Optimize imports

--- dbpool/src/test/java/com/proofpoint/dbpool/TestH2EmbeddedDataSourceModule.java
+++ dbpool/src/test/java/com/proofpoint/dbpool/TestH2EmbeddedDataSourceModule.java
@@ -19,13 +19,9 @@ import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
 import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
 import java.util.HashMap;
 import java.util.Map;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static org.mockito.Mockito.mock;
 import static org.testng.Assert.assertEquals;
@@ -35,14 +31,12 @@ import static org.testng.Assert.assertSame;
 public class TestH2EmbeddedDataSourceModule
 {
     @Retention(RUNTIME)
-    @Target({FIELD, PARAMETER, METHOD})
     @BindingAnnotation
     public @interface MainBinding
     {
     }
 
     @Retention(RUNTIME)
-    @Target({FIELD, PARAMETER, METHOD})
     @BindingAnnotation
     public @interface AliasBinding
     {
