be407cc80e448bb8f3fb8590fc7c5f7e2744ac96
Martin Traverso <mtraverso@acm.org>
2011-01-14 13:33:28 -0800

    delete temp files when test completes

--- jetty/src/test/java/com/proofpoint/jetty/TestJettyModule.java
+++ jetty/src/test/java/com/proofpoint/jetty/TestJettyModule.java
@@ -1,6 +1,8 @@
 package com.proofpoint.jetty;
 
 import com.beust.jcommander.internal.Maps;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.io.Files;
 import com.google.inject.Binder;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -8,17 +10,41 @@ import com.google.inject.Module;
 import com.proofpoint.configuration.ConfigurationFactory;
 import com.proofpoint.configuration.ConfigurationModule;
 import org.eclipse.jetty.server.Server;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.servlet.Servlet;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class TestJettyModule
 {
+    private File tempDir;
+
+    @BeforeMethod
+    public void setup()
+            throws IOException
+    {
+        tempDir = Files.createTempDir().getCanonicalFile(); // getCanonicalFile needed to get around Issue 365 (http://code.google.com/p/guava-libraries/issues/detail?id=365)
+    }
+
+    @AfterMethod
+    public void tearDown()
+            throws IOException
+    {
+        Files.deleteRecursively(tempDir);
+    }
+
     @Test
     public void testCanConstructServer()
+            throws IOException
     {
-        Map<String, String> properties = Maps.newHashMap();
+        Map<String, String> properties = new ImmutableMap.Builder<String, String>()
+                .put("jetty.log.path", new File(tempDir, "jetty.log").getAbsolutePath())
+                .build();
+
         ConfigurationFactory configFactory = new ConfigurationFactory(properties);
         Injector injector = Guice.createInjector(new JettyModule(),
                                                  new ConfigurationModule(configFactory),
