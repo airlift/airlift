72affe49436397e6dc5432a394a90862b5102c2e
Martin Traverso <mtraverso@acm.org>
2011-01-19 20:51:18 -0800

    CR-84: add precondition checks

--- sample-server/src/main/java/com/proofpoint/sample/PersonStore.java
+++ sample-server/src/main/java/com/proofpoint/sample/PersonStore.java
@@ -1,5 +1,6 @@
 package com.proofpoint.sample;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.MapMaker;
 import com.google.inject.Inject;
 
@@ -14,6 +15,8 @@ public class PersonStore
     @Inject
     public PersonStore(StoreConfig config)
     {
+        Preconditions.checkNotNull(config, "config must not be null");
+
         persons = new MapMaker()
                 .expiration((long) config.getTtl().toMillis(), TimeUnit.MILLISECONDS)
                 .makeMap();
@@ -21,16 +24,23 @@ public class PersonStore
 
     public Person get(String id)
     {
+        Preconditions.checkNotNull(id, "id must not be null");
+
         return persons.get(id);
     }
 
     public void put(String id, Person person)
     {
+        Preconditions.checkNotNull(id, "id must not be null");
+        Preconditions.checkNotNull(person, "person must not be null");
+
         persons.put(id, person);
     }
 
     public void delete(String id)
     {
+        Preconditions.checkNotNull(id, "id must not be null");
+
         persons.remove(id);
     }
 
