751a5b9a7558cf4d92fa67224cb145cf18e0a2cd
Dain Sundstrom <dain@iq80.com>
2011-01-20 15:12:15 -0800

    Added stats to sample-server Updated to new jmx-utils

---
+++ sample-server/src/main/java/com/proofpoint/sample/PersonStoreStats.java
@@ -0,0 +1,57 @@
+package com.proofpoint.sample;
+
+import org.weakref.jmx.Managed;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PersonStoreStats
+{
+    private final AtomicLong fetched = new AtomicLong();
+    private final AtomicLong added = new AtomicLong();
+    private final AtomicLong updated = new AtomicLong();
+    private final AtomicLong removed = new AtomicLong();
+
+    @Managed
+    public long getFetched()
+    {
+        return fetched.get();
+    }
+
+    @Managed
+    public long getAdded()
+    {
+        return added.get();
+    }
+
+    @Managed
+    public long getUpdated()
+    {
+        return updated.get();
+    }
+
+    @Managed
+    public long getRemoved()
+    {
+        return removed.get();
+    }
+
+    public void personFetched()
+    {
+        fetched.getAndIncrement();
+    }
+
+    public void personAdded()
+    {
+        added.getAndIncrement();
+    }
+
+    public void personUpdated()
+    {
+        updated.getAndIncrement();
+    }
+
+    public void personRemoved()
+    {
+        removed.getAndIncrement();
+    }
+}
