a55459390764a34758c88758fbe5cc604b213c4a
Dain Sundstrom <dain@iq80.com>
2011-05-06 08:09:41 -0700

    Added builder to ServiceDescriptor for easier construction

--- experimental/src/main/java/com/proofpoint/experimental/discovery/client/ServiceDescriptor.java
+++ experimental/src/main/java/com/proofpoint/experimental/discovery/client/ServiceDescriptor.java
@@ -2,6 +2,7 @@ package com.proofpoint.experimental.discovery.client;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
+import com.proofpoint.node.NodeInfo;
 import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
 
@@ -108,6 +109,85 @@ public class ServiceDescriptor
         sb.append(", properties=").append(properties);
         sb.append('}');
         return sb.toString();
+    }
+
+    public static ServiceDescriptorBuilder serviceDescriptor(String type)
+    {
+        Preconditions.checkNotNull(type, "type is null");
+        return new ServiceDescriptorBuilder(type);
+    }
+
+    public static class ServiceDescriptorBuilder
+    {
+        private UUID id = UUID.randomUUID();
+        private String nodeId;
+        private String type;
+        private String pool = ServiceSelectorConfig.DEFAULT_POOL;
+        private String location;
+
+        private ImmutableMap.Builder<String, String> properties = ImmutableMap.builder();
+
+        private ServiceDescriptorBuilder(String type)
+        {
+            this.type = type;
+        }
+
+        public ServiceDescriptorBuilder setId(UUID id)
+        {
+            Preconditions.checkNotNull(id, "id is null");
+            this.id = id;
+            return this;
+        }
+
+        public ServiceDescriptorBuilder setNodeInfo(NodeInfo nodeInfo)
+        {
+            Preconditions.checkNotNull(nodeInfo, "nodeInfo is null");
+            this.nodeId = nodeInfo.getNodeId();
+            this.pool = nodeInfo.getPool();
+            return this;
+        }
+
+        public ServiceDescriptorBuilder setNodeId(String nodeId)
+        {
+            Preconditions.checkNotNull(nodeId, "nodeId is null");
+            this.nodeId = nodeId;
+            return this;
+        }
+
+
+        public ServiceDescriptorBuilder setPool(String pool)
+        {
+            Preconditions.checkNotNull(pool, "pool is null");
+            this.pool = pool;
+            return this;
+        }
+
+        public ServiceDescriptorBuilder setLocation(String location)
+        {
+            Preconditions.checkNotNull(location, "location is null");
+            this.location = location;
+            return this;
+        }
+
+        public ServiceDescriptorBuilder addProperty(String key, String value)
+        {
+            Preconditions.checkNotNull(key, "key is null");
+            Preconditions.checkNotNull(value, "value is null");
+            properties.put(key, value);
+            return this;
+        }
+
+        public ServiceDescriptorBuilder addProperties(Map<String, String> properties)
+        {
+            Preconditions.checkNotNull(properties, "properties is null");
+            this.properties.putAll(properties);
+            return this;
+        }
+
+        public ServiceDescriptor build()
+        {
+            return new ServiceDescriptor(id, nodeId, type, pool, location, properties.build());
+        }
     }
 }
 
