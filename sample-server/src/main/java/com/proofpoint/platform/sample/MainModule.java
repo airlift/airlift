9d85cb5eb6837d0dfc39869a5f5f37aa92e696df
Dain Sundstrom <dain@iq80.com>
2011-05-15 22:09:02 -0700

    Removed unnecessary import

--- sample-server/src/main/java/com/proofpoint/platform/sample/MainModule.java
+++ sample-server/src/main/java/com/proofpoint/platform/sample/MainModule.java
@@ -18,7 +18,6 @@ package com.proofpoint.platform.sample;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
-import com.proofpoint.discovery.client.DiscoveryBinder;
 import org.weakref.jmx.guice.MBeanModule;
 
 import static com.proofpoint.configuration.ConfigurationModule.bindConfig;
