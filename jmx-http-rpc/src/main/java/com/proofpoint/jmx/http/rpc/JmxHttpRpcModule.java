099d20f8fe28f71ac3c99f4b1c188b4d51bb22e7
Dain Sundstrom <dain@iq80.com>
2011-11-07 10:54:52 -0800

    Announce jmx-http-rpc using the admin base uri

--- jmx-http-rpc/src/main/java/com/proofpoint/jmx/http/rpc/JmxHttpRpcModule.java
+++ jmx-http-rpc/src/main/java/com/proofpoint/jmx/http/rpc/JmxHttpRpcModule.java
@@ -2,23 +2,29 @@ package com.proofpoint.jmx.http.rpc;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Binder;
+import com.google.inject.Inject;
 import com.google.inject.Module;
+import com.google.inject.Provider;
 import com.google.inject.Provides;
 import com.google.inject.Scopes;
 import com.google.inject.TypeLiteral;
+import com.proofpoint.discovery.client.ServiceAnnouncement;
+import com.proofpoint.discovery.client.ServiceAnnouncement.ServiceAnnouncementBuilder;
+import com.proofpoint.http.server.HttpServerInfo;
 import com.proofpoint.http.server.TheAdminServlet;
 
 import javax.servlet.Servlet;
 
 import java.lang.annotation.Annotation;
+import java.net.URI;
 import java.util.Map;
 
 import static com.proofpoint.configuration.ConfigurationModule.bindConfig;
 import static com.proofpoint.discovery.client.DiscoveryBinder.discoveryBinder;
+import static com.proofpoint.discovery.client.ServiceAnnouncement.serviceAnnouncement;
 
 public class JmxHttpRpcModule implements Module
 {
-
     private final Class<? extends Annotation> bindingAnnotation;
 
     public JmxHttpRpcModule()
@@ -38,8 +44,10 @@ public class JmxHttpRpcModule implements Module
         binder.disableCircularProxies();
 
         binder.bind(Servlet.class).annotatedWith(bindingAnnotation).to(MBeanServerServlet.class).in(Scopes.SINGLETON);
-        binder.bind(new TypeLiteral<Map<String, String>>() {}).annotatedWith(bindingAnnotation).toInstance(ImmutableMap.<String,String>of());
-        discoveryBinder(binder).bindHttpAnnouncement("jmx-http-rpc");
+        binder.bind(new TypeLiteral<Map<String, String>>() {}).annotatedWith(bindingAnnotation).toInstance(ImmutableMap.<String, String>of());
+
+        ServiceAnnouncementBuilder serviceAnnouncementBuilder = serviceAnnouncement("jmx-http-rpc");
+        discoveryBinder(binder).bindServiceAnnouncement(new HttpAdminAnnouncementProvider(serviceAnnouncementBuilder));
 
         bindConfig(binder).to(JmxHttpRpcConfig.class);
     }
@@ -48,5 +56,37 @@ public class JmxHttpRpcModule implements Module
     public HttpMBeanServerCredentials createCredentials(JmxHttpRpcConfig config)
     {
         return new HttpMBeanServerCredentials(config.getUsername(), config.getPassword());
+    }
+
+
+    static class HttpAdminAnnouncementProvider implements Provider<ServiceAnnouncement>
+    {
+        private final ServiceAnnouncementBuilder builder;
+        private HttpServerInfo httpServerInfo;
+
+        public HttpAdminAnnouncementProvider(ServiceAnnouncementBuilder serviceAnnouncementBuilder)
+        {
+            builder = serviceAnnouncementBuilder;
+        }
+
+        @Inject
+        public synchronized void setHttpServerInfo(HttpServerInfo httpServerInfo)
+        {
+            this.httpServerInfo = httpServerInfo;
+        }
+
+        @Override
+        public synchronized ServiceAnnouncement get()
+        {
+            if (httpServerInfo.getAdminUri() != null) {
+                URI adminUri = httpServerInfo.getAdminUri();
+                if (adminUri.getScheme().equals("http")) {
+                    builder.addProperty("http", adminUri.toString());
+                } else if (adminUri.getScheme().equals("https")) {
+                    builder.addProperty("https", adminUri.toString());
+                }
+            }
+            return builder.build();
+        }
     }
 }
