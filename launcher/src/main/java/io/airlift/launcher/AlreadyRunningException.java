proofpoint/platform

Address review comments.
Convert Ruby snake_case to camelCase.
Handle -Dproperty switches without following "=".
Better method names PidFile.indicateStarting() and .indicateRunning().
PidFile.indicateStarting() throws AlreadyRunningException instead of returning non-negative value.
Fix typo and improve readability.

launcher/src/main/java/com/proofpoint/launcher/AlreadyRunningException.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright 2013 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.launcher;
+
+public class AlreadyRunningException extends Throwable
+{
+    private final int pid;
+
+    public AlreadyRunningException(int pid)
+    {
+
+        this.pid = pid;
+    }
+
+    @Override
+    public String getMessage()
+    {
+        StringBuilder msg = new StringBuilder("Already running");
+        if (pid != 0) {
+            msg.append(" as ").append(pid);
+        }
+        return msg.toString();
+    }
+}

airlift/airlift

launcher/src/main/java/io/airlift/launcher/AlreadyRunningException.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright 2013 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.launcher;
+
+public class AlreadyRunningException extends Throwable
+{
+    private final int pid;
+
+    public AlreadyRunningException(int pid)
+    {
+
+        this.pid = pid;
+    }
+
+    @Override
+    public String getMessage()
+    {
+        StringBuilder msg = new StringBuilder("Already running");
+        if (pid != 0) {
+            msg.append(" as ").append(pid);
+        }
+        return msg.toString();
+    }
+}