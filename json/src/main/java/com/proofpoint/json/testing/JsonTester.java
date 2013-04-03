7f1b62cc0968183a07da5f41ce7a7cef8976ab49
John Myers <jgmyers@proofpoint.com>
2013-04-02 20:25:36 -0700

    Create JsonTester.assertJsonEncode()

--- json/src/main/java/com/proofpoint/json/testing/JsonTester.java
+++ json/src/main/java/com/proofpoint/json/testing/JsonTester.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.proofpoint.json.testing;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -5,13 +20,36 @@ import com.proofpoint.json.JsonCodec;
 
 import java.io.IOException;
 
+import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
 import static com.google.common.base.Throwables.propagate;
 import static com.proofpoint.json.JsonCodec.jsonCodec;
+import static org.testng.Assert.assertEquals;
 
 public class JsonTester
 {
     private JsonTester()
     {
+    }
+
+    public static <T> void assertJsonEncode(T pojo, Object expected)
+    {
+        assertJsonEncode(pojo, expected, null);
+    }
+
+    public static <T> void assertJsonEncode(T pojo, Object expected, String message)
+    {
+        try {
+            String json = new ObjectMapper().enable(INDENT_OUTPUT).writeValueAsString(pojo);
+            StringBuilder builder = new StringBuilder();
+            if (message != null) {
+                builder.append(message).append(' ');
+            }
+            builder.append("JSON encoding ").append(json);
+            assertEquals(new ObjectMapper().readValue(json, Object.class), expected, builder.toString());
+        }
+        catch (IOException e) {
+            throw propagate(e);
+        }
     }
 
     public static <T> T decodeJson(Class<T> tClass, Object value)
---
+++ json/src/test/java/com/proofpoint/json/testing/TestJsonTester.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2013 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.json.testing;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableList;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.proofpoint.json.testing.JsonTester.assertJsonEncode;
+
+public class TestJsonTester
+{
+
+    private final SimpleEncoder simpleEncoder = new SimpleEncoder();
+    private Map<String, Object> simpleExpected;
+    private final ComplexEncoder complexEncoder = new ComplexEncoder();
+    private Map<String, Object> complexExpected;
+
+    @BeforeMethod
+    public void setup()
+    {
+        simpleExpected = new HashMap<>();
+        simpleExpected.put("s", "fred");
+        simpleExpected.put("i", 3);
+        simpleExpected.put("b", true);
+        simpleExpected.put("nul", null);
+
+        complexExpected = new HashMap<>();
+        complexExpected.put("list", ImmutableList.of("a", "b", "a"));
+        complexExpected.put("obj", simpleExpected);
+    }
+
+    @Test
+    public void testEncodeSimple()
+    {
+        assertJsonEncode(simpleEncoder, simpleExpected);
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void testMissingField()
+    {
+       simpleExpected.put("extra", "field");
+       assertJsonEncode(simpleEncoder, simpleExpected);
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void testExtraField()
+    {
+       simpleExpected.remove("nul");
+       assertJsonEncode(simpleEncoder, simpleExpected);
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void testStringAsNumber()
+    {
+       simpleExpected.put("i", "3");
+       assertJsonEncode(simpleEncoder, simpleExpected);
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void testStringAsBoolean()
+    {
+       simpleExpected.put("b", "true");
+       assertJsonEncode(simpleEncoder, simpleExpected);
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void testStringAsNull()
+    {
+       simpleExpected.put("nul", "null");
+       assertJsonEncode(simpleEncoder, simpleExpected);
+    }
+    
+    @Test
+    public void testEncodeComplex()
+    {
+        assertJsonEncode(complexEncoder, complexExpected);
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void testWrongListMember()
+    {
+        complexExpected.put("list", ImmutableList.of("a", "b", "b"));
+        assertJsonEncode(complexEncoder, complexExpected);
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void testWrongListOrder()
+    {
+        complexExpected.put("list", ImmutableList.of("a", "a", "b"));
+        assertJsonEncode(complexEncoder, complexExpected);
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void testMissingListMember()
+    {
+        complexExpected.put("list", ImmutableList.of("a", "b"));
+        assertJsonEncode(complexEncoder, complexExpected);
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void testSubObjWithWrongValue()
+    {
+        simpleExpected.put("s", "wrong");
+        assertJsonEncode(complexEncoder, complexExpected);
+    }
+
+    private class SimpleEncoder
+    {
+        @JsonProperty
+        private final String s = "fred";
+        @JsonProperty
+        private final int i = 3;
+        @JsonProperty
+        private final boolean b = true;
+        @JsonProperty
+        private final Integer nul = null;
+    }
+
+    private class ComplexEncoder
+    {
+        @JsonProperty
+        private final List<String> list = ImmutableList.of("a", "b", "a");
+        @JsonProperty
+        private final SimpleEncoder obj = new SimpleEncoder();
+    }
+}
