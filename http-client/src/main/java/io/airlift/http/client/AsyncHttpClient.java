1cbbba397847f00938c9b23231e9e454df55bd29
John Myers <jgmyers@proofpoint.com>
2013-04-27 12:32:19 -0700

    Simplify AsyncHttpClient
    Callers of AsyncHttpClient.executeAsync() shouldn't need to write unnecessary exception handling code around the call. Instead they should rely on any ResponseHandler exception being delivered through the AsyncHttpResponseFuture

--- http-client/src/main/java/io/airlift/http/client/AsyncHttpClient.java
+++ http-client/src/main/java/io/airlift/http/client/AsyncHttpClient.java
@@ -7,8 +7,7 @@ import com.google.common.util.concurrent.CheckedFuture;
 public interface AsyncHttpClient
         extends HttpClient
 {
-    <T, E extends Exception> AsyncHttpResponseFuture<T, E> executeAsync(Request request, ResponseHandler<T, E> responseHandler)
-            throws E;
+    <T, E extends Exception> AsyncHttpResponseFuture<T, E> executeAsync(Request request, ResponseHandler<T, E> responseHandler);
 
     public interface AsyncHttpResponseFuture<T, E extends Exception>
             extends CheckedFuture<T, E>
