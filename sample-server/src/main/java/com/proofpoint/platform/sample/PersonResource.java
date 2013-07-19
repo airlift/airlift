proofpoint/platform

Cleanup sample server

sample-server/src/main/java/com/proofpoint/platform/sample/PersonResource.java
@@ -31,7 +31,7 @@ import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.proofpoint.platform.sample.PersonWithSelf.from;
+import static com.proofpoint.platform.sample.PersonWithSelf.createPersonSelf;
 
 @Path("/v1/person/{id: \\w+}")
 public class PersonResource
@@ -58,7 +58,7 @@ public class PersonResource
             return Response.status(Response.Status.NOT_FOUND).entity("[" + id + "]").build();
         }
 
-        return Response.ok(from(person, uriInfo.getRequestUri())).build();
+        return Response.ok(createPersonSelf(person, uriInfo.getRequestUri())).build();
     }
 
     @PUT

sample-server/src/main/java/com/proofpoint/platform/sample/PersonWithSelf.java
@@ -27,7 +27,7 @@ public class PersonWithSelf
     private final Person person;
     private final URI self;
 
-    public static PersonWithSelf from(Person person, URI self)
+    public static PersonWithSelf createPersonSelf(Person person, URI self)
     {
         return new PersonWithSelf(person, self);
     }

sample-server/src/main/java/com/proofpoint/platform/sample/PersonsResource.java
@@ -31,7 +31,7 @@ import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.proofpoint.platform.sample.PersonWithSelf.from;
+import static com.proofpoint.platform.sample.PersonWithSelf.createPersonSelf;
 
 @Path("/v1/person")
 public class PersonsResource
@@ -53,7 +53,7 @@ public class PersonsResource
         Builder<PersonWithSelf> builder = ImmutableList.builder();
         for (StoreEntry entry : store.getAll()) {
             URI self = UriBuilder.fromUri(uriInfo.getRequestUri()).path(entry.getId()).build();
-            builder.add(from(entry.getPerson(), self));
+            builder.add(createPersonSelf(entry.getPerson(), self));
         }
         return Response.ok(builder.build()).build();
     }

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonResource.java
@@ -27,7 +27,7 @@ import java.net.URI;
 import static com.proofpoint.platform.sample.PersonEvent.personAdded;
 import static com.proofpoint.platform.sample.PersonEvent.personRemoved;
 import static com.proofpoint.platform.sample.PersonEvent.personUpdated;
-import static com.proofpoint.platform.sample.PersonWithSelf.from;
+import static com.proofpoint.platform.sample.PersonWithSelf.createPersonSelf;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
 
@@ -59,7 +59,7 @@ public class TestPersonResource
 
         Response response = resource.get("foo", MockUriInfo.from(URI.create("http://localhost/v1/person/1")));
         assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
-        assertEquals(response.getEntity(), from(new Person("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/1")));
+        assertEquals(response.getEntity(), createPersonSelf(new Person("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/1")));
         assertNull(response.getMetadata().get("Content-Type")); // content type is set by jersey based on @Produces
     }
 

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonWithSelf.java
@@ -21,6 +21,7 @@ import org.testng.annotations.Test;
 import java.net.URI;
 
 import static com.proofpoint.json.testing.JsonTester.assertJsonEncode;
+import static com.proofpoint.platform.sample.PersonWithSelf.createPersonSelf;
 import static com.proofpoint.testing.EquivalenceTester.equivalenceTester;
 
 public class TestPersonWithSelf
@@ -33,17 +34,17 @@ public class TestPersonWithSelf
         Person barPerson = new Person("bar@example.com", "Mr Bar");
         Person barPerson2 = new Person("bar@example.com", "Mr Bar");
         equivalenceTester()
-                .addEquivalentGroup(PersonWithSelf.from(fooPerson, URI.create("http://example.com")), PersonWithSelf.from(fooPerson2, URI.create("http://example.com")))
-                .addEquivalentGroup(PersonWithSelf.from(fooPerson, URI.create("https://example.com")), PersonWithSelf.from(fooPerson2, URI.create("https://example.com")))
-                .addEquivalentGroup(PersonWithSelf.from(barPerson, URI.create("http://example.com")), PersonWithSelf.from(barPerson2, URI.create("http://example.com")))
-                .addEquivalentGroup(PersonWithSelf.from(barPerson, URI.create("https://example.com")), PersonWithSelf.from(barPerson2, URI.create("https://example.com")))
+                .addEquivalentGroup(createPersonSelf(fooPerson, URI.create("http://example.com")), createPersonSelf(fooPerson2, URI.create("http://example.com")))
+                .addEquivalentGroup(createPersonSelf(fooPerson, URI.create("https://example.com")), createPersonSelf(fooPerson2, URI.create("https://example.com")))
+                .addEquivalentGroup(createPersonSelf(barPerson, URI.create("http://example.com")), createPersonSelf(barPerson2, URI.create("http://example.com")))
+                .addEquivalentGroup(createPersonSelf(barPerson, URI.create("https://example.com")), createPersonSelf(barPerson2, URI.create("https://example.com")))
                 .check();
     }
 
     @Test
     public void testJsonEncode()
     {
-        assertJsonEncode(PersonWithSelf.from(new Person("alice@example.com", "Alice"), URI.create("http://example.com/foo")),
+        assertJsonEncode(createPersonSelf(new Person("alice@example.com", "Alice"), URI.create("http://example.com/foo")),
                 ImmutableMap.of(
                         "self", "http://example.com/foo",
                         "name", "Alice",

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonsResource.java
@@ -25,7 +25,7 @@ import java.net.URI;
 import java.util.Collection;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static com.proofpoint.platform.sample.PersonWithSelf.from;
+import static com.proofpoint.platform.sample.PersonWithSelf.createPersonSelf;
 import static com.proofpoint.testing.Assertions.assertInstanceOf;
 import static org.testng.Assert.assertEquals;
 
@@ -60,10 +60,8 @@ public class TestPersonsResource
         assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
         assertInstanceOf(response.getEntity(), Collection.class);
         assertEquals((Collection<?>) response.getEntity(), newArrayList(
-                from(new Person("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/foo")),
-                from(new Person("bar@example.com", "Mr Bar"), URI.create("http://localhost/v1/person/bar"))
+                createPersonSelf(new Person("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/foo")),
+                createPersonSelf(new Person("bar@example.com", "Mr Bar"), URI.create("http://localhost/v1/person/bar"))
         ));
     }
-
-
 }

airlift/airlift

sample-server/src/main/java/com/proofpoint/platform/sample/PersonResource.java
@@ -31,7 +31,7 @@ import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.proofpoint.platform.sample.PersonWithSelf.from;
+import static com.proofpoint.platform.sample.PersonWithSelf.createPersonWithSelf;
 
 @Path("/v1/person/{id: \\w+}")
 public class PersonResource
@@ -58,7 +58,7 @@ public class PersonResource
             return Response.status(Response.Status.NOT_FOUND).entity("[" + id + "]").build();
         }
 
-        return Response.ok(from(person, uriInfo.getRequestUri())).build();
+        return Response.ok(createPersonWithSelf(person, uriInfo.getRequestUri())).build();
     }
 
     @PUT

sample-server/src/main/java/com/proofpoint/platform/sample/PersonWithSelf.java
@@ -27,7 +27,7 @@ public class PersonWithSelf
     private final Person person;
     private final URI self;
 
-    public static PersonWithSelf from(Person person, URI self)
+    public static PersonWithSelf createPersonWithSelf(Person person, URI self)
     {
         return new PersonWithSelf(person, self);
     }

sample-server/src/main/java/com/proofpoint/platform/sample/PersonsResource.java
@@ -31,7 +31,7 @@ import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.proofpoint.platform.sample.PersonWithSelf.from;
+import static com.proofpoint.platform.sample.PersonWithSelf.createPersonWithSelf;
 
 @Path("/v1/person")
 public class PersonsResource
@@ -53,7 +53,7 @@ public class PersonsResource
         Builder<PersonWithSelf> builder = ImmutableList.builder();
         for (StoreEntry entry : store.getAll()) {
             URI self = UriBuilder.fromUri(uriInfo.getRequestUri()).path(entry.getId()).build();
-            builder.add(from(entry.getPerson(), self));
+            builder.add(createPersonWithSelf(entry.getPerson(), self));
         }
         return Response.ok(builder.build()).build();
     }

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonResource.java
@@ -27,7 +27,7 @@ import java.net.URI;
 import static com.proofpoint.platform.sample.PersonEvent.personAdded;
 import static com.proofpoint.platform.sample.PersonEvent.personRemoved;
 import static com.proofpoint.platform.sample.PersonEvent.personUpdated;
-import static com.proofpoint.platform.sample.PersonWithSelf.from;
+import static com.proofpoint.platform.sample.PersonWithSelf.createPersonWithSelf;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
 
@@ -59,7 +59,7 @@ public class TestPersonResource
 
         Response response = resource.get("foo", MockUriInfo.from(URI.create("http://localhost/v1/person/1")));
         assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
-        assertEquals(response.getEntity(), from(new Person("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/1")));
+        assertEquals(response.getEntity(), createPersonWithSelf(new Person("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/1")));
         assertNull(response.getMetadata().get("Content-Type")); // content type is set by jersey based on @Produces
     }
 

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonWithSelf.java
@@ -21,6 +21,7 @@ import org.testng.annotations.Test;
 import java.net.URI;
 
 import static com.proofpoint.json.testing.JsonTester.assertJsonEncode;
+import static com.proofpoint.platform.sample.PersonWithSelf.createPersonWithSelf;
 import static com.proofpoint.testing.EquivalenceTester.equivalenceTester;
 
 public class TestPersonWithSelf
@@ -33,17 +34,17 @@ public class TestPersonWithSelf
         Person barPerson = new Person("bar@example.com", "Mr Bar");
         Person barPerson2 = new Person("bar@example.com", "Mr Bar");
         equivalenceTester()
-                .addEquivalentGroup(PersonWithSelf.from(fooPerson, URI.create("http://example.com")), PersonWithSelf.from(fooPerson2, URI.create("http://example.com")))
-                .addEquivalentGroup(PersonWithSelf.from(fooPerson, URI.create("https://example.com")), PersonWithSelf.from(fooPerson2, URI.create("https://example.com")))
-                .addEquivalentGroup(PersonWithSelf.from(barPerson, URI.create("http://example.com")), PersonWithSelf.from(barPerson2, URI.create("http://example.com")))
-                .addEquivalentGroup(PersonWithSelf.from(barPerson, URI.create("https://example.com")), PersonWithSelf.from(barPerson2, URI.create("https://example.com")))
+                .addEquivalentGroup(createPersonWithSelf(fooPerson, URI.create("http://example.com")), createPersonWithSelf(fooPerson2, URI.create("http://example.com")))
+                .addEquivalentGroup(createPersonWithSelf(fooPerson, URI.create("https://example.com")), createPersonWithSelf(fooPerson2, URI.create("https://example.com")))
+                .addEquivalentGroup(createPersonWithSelf(barPerson, URI.create("http://example.com")), createPersonWithSelf(barPerson2, URI.create("http://example.com")))
+                .addEquivalentGroup(createPersonWithSelf(barPerson, URI.create("https://example.com")), createPersonWithSelf(barPerson2, URI.create("https://example.com")))
                 .check();
     }
 
     @Test
     public void testJsonEncode()
     {
-        assertJsonEncode(PersonWithSelf.from(new Person("alice@example.com", "Alice"), URI.create("http://example.com/foo")),
+        assertJsonEncode(createPersonWithSelf(new Person("alice@example.com", "Alice"), URI.create("http://example.com/foo")),
                 ImmutableMap.of(
                         "self", "http://example.com/foo",
                         "name", "Alice",

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonsResource.java
@@ -25,7 +25,7 @@ import java.net.URI;
 import java.util.Collection;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static com.proofpoint.platform.sample.PersonWithSelf.from;
+import static com.proofpoint.platform.sample.PersonWithSelf.createPersonWithSelf;
 import static com.proofpoint.testing.Assertions.assertInstanceOf;
 import static org.testng.Assert.assertEquals;
 
@@ -60,10 +60,8 @@ public class TestPersonsResource
         assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
         assertInstanceOf(response.getEntity(), Collection.class);
         assertEquals((Collection<?>) response.getEntity(), newArrayList(
-                from(new Person("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/foo")),
-                from(new Person("bar@example.com", "Mr Bar"), URI.create("http://localhost/v1/person/bar"))
+                createPersonWithSelf(new Person("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/foo")),
+                createPersonWithSelf(new Person("bar@example.com", "Mr Bar"), URI.create("http://localhost/v1/person/bar"))
         ));
     }
-
-
 }