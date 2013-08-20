1308ede85ed6dfba8d801e9479d83d5660787486
John Myers <jgmyers@proofpoint.com>
2013-08-20 11:07:00 -0700

    Create ReportCollectionFactory

---
+++ reporting/src/main/java/com/proofpoint/reporting/ReportCollectionFactory.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2013 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.reporting;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Ticker;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.google.inject.Inject;
+import org.weakref.jmx.MBeanExporter;
+import org.weakref.jmx.ObjectNameBuilder;
+
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.reflect.Proxy.newProxyInstance;
+
+class ReportCollectionFactory
+{
+    private final Ticker ticker;
+    private final MBeanExporter mBeanExporter;
+    private final ReportExporter reportExporter;
+
+    @Inject
+    public ReportCollectionFactory(MBeanExporter mBeanExporter, ReportExporter reportExporter)
+    {
+        this(mBeanExporter, reportExporter, Ticker.systemTicker());
+    }
+
+    ReportCollectionFactory(MBeanExporter mBeanExporter, ReportExporter reportExporter, Ticker ticker)
+    {
+        this.mBeanExporter = mBeanExporter;
+        this.reportExporter = reportExporter;
+        this.ticker = ticker;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T createReportCollection(Class<T> aClass)
+    {
+        return (T) newProxyInstance(aClass.getClassLoader(),
+                new Class[]{aClass},
+                new StatInvocationHandler(aClass));
+    }
+
+    private class StatInvocationHandler implements InvocationHandler
+    {
+        private final Map<Method,LoadingCache<List<Optional<String>>,Object>> cacheMap;
+        private final ConcurrentMap<Object, String> objectNameMap = new ConcurrentHashMap<>();
+
+        public <T> StatInvocationHandler(Class<T> aClass)
+        {
+            Builder<Method, LoadingCache<List<Optional<String>>, Object>> cacheBuilder = ImmutableMap.builder();
+
+            for (final Method method : aClass.getMethods()) {
+                final Constructor<?> constructor;
+                try {
+                    constructor = method.getReturnType().getConstructor();
+                }
+                catch (NoSuchMethodException e) {
+                    throw new RuntimeException(methodName(method) + " return type " + method.getReturnType().getSimpleName()
+                            + " has no public no-arg constructor");
+                }
+
+                ImmutableList.Builder<String> keyNameBuilder = ImmutableList.builder();
+                int argPosition = 0;
+                for (Annotation[] annotations : method.getParameterAnnotations()) {
+                    ++argPosition;
+                    boolean found = false;
+                    for (Annotation annotation : annotations) {
+                        if (Key.class.isAssignableFrom(annotation.annotationType())) {
+                            keyNameBuilder.add(((Key)annotation).value());
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        throw new RuntimeException(methodName(method) + " parameter " + argPosition
+                                + " has no @com.proofpoint.reporting.Key annotation");
+                    }
+                }
+                final String packageName = aClass.getPackage().getName();
+                final String className = aClass.getSimpleName();
+                final String methodName = method.getName();
+                final List<String> keyNames = keyNameBuilder.build();
+
+                cacheBuilder.put(method, CacheBuilder.newBuilder()
+                        .ticker(ticker)
+                        .expireAfterAccess(15, TimeUnit.MINUTES)
+                        .removalListener(new UnexportRemovalListener())
+                        .build(new CacheLoader<List<Optional<String>>, Object>()
+                        {
+                            @Override
+                            public Object load(List<Optional<String>> key)
+                                    throws Exception
+                            {
+                                Object stat = constructor.newInstance();
+
+                                ObjectNameBuilder objectNameBuilder = new ObjectNameBuilder(packageName)
+                                        .withProperty("type", className)
+                                        .withProperty("name", methodName);
+                                for (int i = 0; i < keyNames.size(); ++i) {
+                                    if (key.get(i).isPresent()) {
+                                        objectNameBuilder = objectNameBuilder.withProperty(keyNames.get(i), key.get(i).get());
+                                    }
+                                }
+                                String objectName = objectNameBuilder.build();
+                                reportExporter.export(ObjectName.getInstance(objectName), stat);
+                                mBeanExporter.export(objectName, stat);
+                                objectNameMap.put(stat, objectName);
+                                return stat;
+                            }
+                        })
+                );
+            }
+            cacheMap = cacheBuilder.build();
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args)
+                throws Throwable
+        {
+            ImmutableList.Builder<Optional<String>> argBuilder = ImmutableList.builder();
+            for (Object arg : args) {
+                if (arg == null) {
+                    argBuilder.add(Optional.<String>absent());
+                }
+                else {
+                    argBuilder.add(Optional.of(arg.toString()));
+                }
+            }
+            return cacheMap.get(method).get(argBuilder.build());
+        }
+
+        private class UnexportRemovalListener implements RemovalListener<List<Optional<String>>, Object>
+        {
+            @Override
+            public void onRemoval(RemovalNotification<List<Optional<String>>, Object> notification)
+            {
+                String objectName = objectNameMap.remove(notification.getValue());
+                try {
+                    reportExporter.unexport(ObjectName.getInstance(objectName));
+                }
+                catch (MalformedObjectNameException ignored) {
+                }
+                mBeanExporter.unexport(objectName);
+            }
+        }
+    }
+
+    private String methodName(Method method)
+    {
+        StringBuilder builder = new StringBuilder(method.getDeclaringClass().getName());
+        builder.append(".").append(method.getName()).append('(');
+        boolean first = true;
+        for (Class<?> type : method.getParameterTypes()) {
+            if (!first) {
+                builder.append(", ");
+            }
+            builder.append(type.getName());
+            first = false;
+        }
+        builder.append(')');
+        return builder.toString();
+    }
+}
---
+++ reporting/src/main/test/com/proofpoint/reporting/TestReportCollectionFactory.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2013 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.reporting;
+
+import com.google.common.base.Ticker;
+import org.mockito.ArgumentCaptor;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.weakref.jmx.MBeanExporter;
+
+import javax.management.ObjectName;
+import javax.validation.constraints.NotNull;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertSame;
+
+public class TestReportCollectionFactory
+{
+    private MBeanExporter mBeanExporter;
+    private ReportExporter reportExporter;
+    private TestTicker ticker;
+    private ReportCollectionFactory reportCollectionFactory;
+
+    @BeforeMethod
+    public void setup()
+    {
+        mBeanExporter = mock(MBeanExporter.class);
+        reportExporter = mock(ReportExporter.class);
+        ticker = new TestTicker();
+        reportCollectionFactory = new ReportCollectionFactory(mBeanExporter, reportExporter, ticker);
+    }
+
+    @Test
+    public void testKeyedDistribution()
+            throws Exception
+    {
+        KeyedDistribution keyedDistribution = reportCollectionFactory.createReportCollection(KeyedDistribution.class);
+        keyedDistribution.add("value", false);
+
+        ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<ObjectName> objectNameCaptor = ArgumentCaptor.forClass(ObjectName.class);
+        ArgumentCaptor<SomeObject> mbeanCaptor = ArgumentCaptor.forClass(SomeObject.class);
+        ArgumentCaptor<SomeObject> reportCaptor = ArgumentCaptor.forClass(SomeObject.class);
+
+        verify(mBeanExporter).export(stringCaptor.capture(), mbeanCaptor.capture());
+        assertEquals(stringCaptor.getValue(), "com.proofpoint.reporting:type=KeyedDistribution,name=add,foo=value,bar=false");
+        verify(reportExporter).export(objectNameCaptor.capture(), reportCaptor.capture());
+        assertEquals(objectNameCaptor.getValue().getCanonicalName(), "com.proofpoint.reporting:bar=false,foo=value,name=add,type=KeyedDistribution");
+        assertSame(mbeanCaptor.getValue(), reportCaptor.getValue());
+    }
+
+    @Test
+    public void testNullValue()
+            throws Exception
+    {
+        KeyedDistribution keyedDistribution = reportCollectionFactory.createReportCollection(KeyedDistribution.class);
+        keyedDistribution.add(null, false);
+
+        ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<ObjectName> objectNameCaptor = ArgumentCaptor.forClass(ObjectName.class);
+        ArgumentCaptor<SomeObject> mbeanCaptor = ArgumentCaptor.forClass(SomeObject.class);
+        ArgumentCaptor<SomeObject> reportCaptor = ArgumentCaptor.forClass(SomeObject.class);
+
+        verify(mBeanExporter).export(stringCaptor.capture(), mbeanCaptor.capture());
+        assertEquals(stringCaptor.getValue(), "com.proofpoint.reporting:type=KeyedDistribution,name=add,bar=false");
+        verify(reportExporter).export(objectNameCaptor.capture(), reportCaptor.capture());
+        assertEquals(objectNameCaptor.getValue().getCanonicalName(), "com.proofpoint.reporting:bar=false,name=add,type=KeyedDistribution");
+        assertSame(mbeanCaptor.getValue(), reportCaptor.getValue());
+    }
+
+    @Test
+    public void testExpiration()
+            throws Exception
+    {
+        KeyedDistribution keyedDistribution = reportCollectionFactory.createReportCollection(KeyedDistribution.class);
+        keyedDistribution.add("value", false);
+        ticker.advance(5, TimeUnit.MINUTES);
+        keyedDistribution.add("value", false);
+        ticker.advance(14, TimeUnit.MINUTES);
+        ticker.advance(59, TimeUnit.SECONDS);
+        keyedDistribution.add("value", true);
+
+        verify(mBeanExporter, never()).unexport(any(String.class));
+        verify(reportExporter, never()).unexport(any(ObjectName.class));
+
+        ticker.advance(1, TimeUnit.SECONDS);
+        keyedDistribution.add("value2", true);
+
+        ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<ObjectName> objectNameCaptor = ArgumentCaptor.forClass(ObjectName.class);
+
+        verify(mBeanExporter).unexport(stringCaptor.capture());
+        assertEquals(stringCaptor.getValue(), "com.proofpoint.reporting:type=KeyedDistribution,name=add,foo=value,bar=false");
+        verify(reportExporter).unexport(objectNameCaptor.capture());
+        assertEquals(objectNameCaptor.getValue().getCanonicalName(), "com.proofpoint.reporting:bar=false,foo=value,name=add,type=KeyedDistribution");
+    }
+
+    private interface KeyedDistribution
+    {
+        SomeObject add(@Key("foo") String key, @NotNull @Key("bar") boolean bool);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class,
+            expectedExceptionsMessageRegExp = "com\\.proofpoint\\.reporting\\.TestReportCollectionFactory\\$MissingParameterName\\.add\\(java\\.lang\\.String, boolean\\) parameter 2 has no @com.proofpoint.reporting.Key annotation")
+    public void testNoParameterNames()
+    {
+        reportCollectionFactory.createReportCollection(MissingParameterName.class);
+    }
+
+    private interface MissingParameterName
+    {
+        SomeObject add(@Key("foo") String key, boolean bool);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class,
+            expectedExceptionsMessageRegExp = "com\\.proofpoint\\.reporting\\.TestReportCollectionFactory\\$NotConstructable\\.add\\(java\\.lang\\.String, boolean\\) return type ConstructorNeedsArgument has no public no-arg constructor")
+    public void testReturnTypeNotConstructable()
+    {
+        reportCollectionFactory.createReportCollection(NotConstructable.class);
+    }
+
+    private interface NotConstructable
+    {
+        ConstructorNeedsArgument add(@Key("foo") String key, @Key("bar") boolean bool);
+    }
+
+    public static class SomeObject
+    {
+    }
+
+    public static class ConstructorNeedsArgument
+    {
+        public ConstructorNeedsArgument(int something)
+        {
+        }
+    }
+
+    private static class TestTicker extends Ticker
+    {
+        private long nanos = 0;
+
+        @Override
+        public long read()
+        {
+            return nanos;
+        }
+
+        public void advance(int amount, TimeUnit unit)
+        {
+            nanos += unit.toNanos(amount);
+        }
+    }
+}
