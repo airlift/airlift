b995a05ac1815c37e97817b32305672c0740fa68
David Phillips <david@acz.org>
2013-11-22 19:16:01 -0800

    Allow using resources with TestingHttpServer

--- http-server/src/test/java/io/airlift/http/server/testing/TestTestingHttpServer.java
+++ http-server/src/test/java/io/airlift/http/server/testing/TestTestingHttpServer.java
@@ -17,6 +17,8 @@ package io.airlift.http.server.testing;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.net.HttpHeaders;
+import com.google.common.net.MediaType;
 import com.google.inject.Binder;
 import com.google.inject.Injector;
 import com.google.inject.Module;
@@ -26,7 +28,10 @@ import io.airlift.bootstrap.LifeCycleManager;
 import io.airlift.http.client.ApacheHttpClient;
 import io.airlift.http.client.HttpClient;
 import io.airlift.http.client.HttpClientConfig;
+import io.airlift.http.client.HttpStatus;
+import io.airlift.http.client.HttpUriBuilder;
 import io.airlift.http.client.StatusResponseHandler.StatusResponse;
+import io.airlift.http.client.StringResponseHandler;
 import io.airlift.http.server.HttpServerConfig;
 import io.airlift.http.server.HttpServerInfo;
 import io.airlift.http.server.TheServlet;
@@ -48,15 +53,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static com.google.common.net.MediaType.PLAIN_TEXT_UTF_8;
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
+import static io.airlift.http.client.HttpUriBuilder.uriBuilderFrom;
 import static io.airlift.http.client.Request.Builder.prepareGet;
 import static io.airlift.http.client.StatusResponseHandler.createStatusResponseHandler;
+import static io.airlift.http.client.StringResponseHandler.createStringResponseHandler;
+import static io.airlift.http.server.HttpServerBinder.HttpResourceBinding;
+import static io.airlift.http.server.HttpServerBinder.httpServerBinder;
 import static io.airlift.testing.Assertions.assertGreaterThan;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class TestTestingHttpServer
 {
@@ -161,7 +173,6 @@ public class TestTestingHttpServer
             lifeCycleManager.stop();
         }
     }
-
     @Test
     public void testGuiceInjectionWithFilters()
             throws Exception
@@ -204,6 +215,68 @@ public class TestTestingHttpServer
         }
     }
 
+    @Test
+    public void testGuiceInjectionWithResources()
+            throws Exception
+    {
+        final DummyServlet servlet = new DummyServlet();
+
+        Bootstrap app = new Bootstrap(
+                new TestingNodeModule(),
+                new TestingHttpServerModule(),
+                new Module()
+                {
+                    @Override
+                    public void configure(Binder binder)
+                    {
+                        binder.bind(Servlet.class).annotatedWith(TheServlet.class).toInstance(servlet);
+                        binder.bind(new TypeLiteral<Map<String, String>>() {}).annotatedWith(TheServlet.class).toInstance(ImmutableMap.<String, String>of());
+                        httpServerBinder(binder).bindResource("/", "webapp/user").withWelcomeFile("user-welcome.txt");
+                        httpServerBinder(binder).bindResource("/", "webapp/user2");
+                        httpServerBinder(binder).bindResource("path", "webapp/user").withWelcomeFile("user-welcome.txt");
+                        httpServerBinder(binder).bindResource("path", "webapp/user2");
+                    }
+                });
+
+        Injector injector = app
+                .strictConfig()
+                .doNotInitializeLogging()
+                .initialize();
+
+        LifeCycleManager lifeCycleManager = injector.getInstance(LifeCycleManager.class);
+        TestingHttpServer server = injector.getInstance(TestingHttpServer.class);
+
+        try (HttpClient client = new ApacheHttpClient(new HttpClientConfig().setConnectTimeout(new Duration(1, SECONDS)))) {
+            // test http resources
+            URI uri = server.getBaseUrl();
+            assertResource(uri, client, "", "welcome user!");
+            assertResource(uri, client, "user-welcome.txt", "welcome user!");
+            assertResource(uri, client, "user.txt", "user");
+            assertResource(uri, client, "user2.txt", "user2");
+            assertResource(uri, client, "path", "welcome user!");
+            assertResource(uri, client, "path/", "welcome user!");
+            assertResource(uri, client, "path/user-welcome.txt", "welcome user!");
+            assertResource(uri, client, "path/user.txt", "user");
+            assertResource(uri, client, "path/user2.txt", "user2");
+
+            // verify that servlet did not receive resource requests
+            assertEquals(servlet.getCallCount(), 0);
+        }
+        finally {
+            lifeCycleManager.stop();
+        }
+    }
+
+    private static void assertResource(URI baseUri, HttpClient client, String path, String contents)
+    {
+        HttpUriBuilder uriBuilder = uriBuilderFrom(baseUri);
+        StringResponseHandler.StringResponse data = client.execute(prepareGet().setUri(uriBuilder.appendPath(path).build()).build(), createStringResponseHandler());
+        assertEquals(data.getStatusCode(), HttpStatus.OK.code());
+        MediaType contentType = MediaType.parse(data.getHeader(HttpHeaders.CONTENT_TYPE));
+        assertTrue(PLAIN_TEXT_UTF_8.is(contentType), "Expected text/plain but got " + contentType);
+        assertEquals(data.getBody().trim(), contents);
+    }
+
     private TestingHttpServer createTestingHttpServer(DummyServlet servlet, Map<String, String> params)
             throws IOException
     {
@@ -219,7 +292,7 @@ public class TestTestingHttpServer
         NodeInfo nodeInfo = new NodeInfo("test");
         HttpServerConfig config = new HttpServerConfig().setHttpPort(0);
         HttpServerInfo httpServerInfo = new HttpServerInfo(config, nodeInfo);
-        return new TestingHttpServer(httpServerInfo, nodeInfo, config, servlet, params, ImmutableSet.<Filter>of(filter));
+        return new TestingHttpServer(httpServerInfo, nodeInfo, config, servlet, params, ImmutableSet.<Filter>of(filter), ImmutableSet.<HttpResourceBinding>of());
     }
 
     static class DummyServlet
