aed047608b09b2acdcf883af221a305258da6e3f
John Myers <jgmyers@proofpoint.com>
2014-03-07 12:44:12 -0800

    Fix threading bug in ReportCollectionFactory
    The RemovalListener for a Gauva LoadingCache is run after the lock is dropped,
     so an expired object could be reinserted and re-registered before the removal
     listener could de-register it.
    
     Use additional data structures to detect this case and prevent the duplicate
     registration and subsequent deregistration.

--- reporting/src/main/java/com/proofpoint/reporting/ReportCollectionFactory.java
+++ reporting/src/main/java/com/proofpoint/reporting/ReportCollectionFactory.java
@@ -29,18 +29,21 @@ import com.google.inject.Inject;
 import org.weakref.jmx.ObjectNameBuilder;
 
 import javax.annotation.Nullable;
+import javax.annotation.concurrent.GuardedBy;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.CaseFormat.LOWER_CAMEL;
@@ -87,121 +90,13 @@ public class ReportCollectionFactory
 
     private class StatInvocationHandler implements InvocationHandler
     {
-        private final Map<Method,LoadingCache<List<Optional<String>>,Object>> cacheMap;
-        private final ConcurrentMap<Object, String> objectNameMap = new ConcurrentHashMap<>();
+        private final Map<Method, PerMethodCache> cacheMap;
 
         public <T> StatInvocationHandler(Class<T> aClass, @Nullable String name)
         {
-            Builder<Method, LoadingCache<List<Optional<String>>, Object>> cacheBuilder = ImmutableMap.builder();
-
-            for (final Method method : aClass.getMethods()) {
-                final Constructor<?> constructor;
-                try {
-                    constructor = method.getReturnType().getConstructor();
-                }
-                catch (NoSuchMethodException e) {
-                    throw new RuntimeException(methodName(method) + " return type " + method.getReturnType().getSimpleName()
-                            + " has no public no-arg constructor");
-                }
-
-                ImmutableList.Builder<String> keyNameBuilder = ImmutableList.builder();
-                int argPosition = 0;
-                for (Annotation[] annotations : method.getParameterAnnotations()) {
-                    ++argPosition;
-                    boolean found = false;
-                    for (Annotation annotation : annotations) {
-                        if (Key.class.isAssignableFrom(annotation.annotationType())) {
-                            keyNameBuilder.add(((Key)annotation).value());
-                            found = true;
-                            break;
-                        }
-                    }
-                    if (!found) {
-                        throw new RuntimeException(methodName(method) + " parameter " + argPosition
-                                + " has no @com.proofpoint.reporting.Key annotation");
-                    }
-                }
-                final String packageName;
-                final Map<String, String> properties = new LinkedHashMap<>();
-                if (name == null) {
-                    packageName = aClass.getPackage().getName();
-                    properties.put("type", aClass.getSimpleName());
-                }
-                else {
-                    ObjectName objectName;
-                    try {
-                        objectName = ObjectName.getInstance(name);
-                    }
-                    catch (MalformedObjectNameException e) {
-                        throw propagate(e);
-                    }
-                    packageName = objectName.getDomain();
-                    int index = packageName.length();
-                    if (name.charAt(index++) != ':') {
-                        throw new RuntimeException("Unable to parse ObjectName " + name);
-                    }
-                    while (index < name.length()) {
-                        int separatorIndex = name.indexOf('=', index);
-                        String key = name.substring(index, separatorIndex);
-                        String value;
-                        if (name.charAt(++separatorIndex) == '\"') {
-                            StringBuilder sb = new StringBuilder();
-                            char c;
-                            while ((c = name.charAt(++separatorIndex)) != '\"') {
-                                if (c == '\\') {
-                                    c = name.charAt(++separatorIndex);
-                                }
-                                sb.append(c);
-                            }
-                            if (name.charAt(++separatorIndex) != ',') {
-                                throw new RuntimeException("Unable to parse ObjectName " + name);
-                            }
-                            value = sb.toString();
-                            index = separatorIndex + 1;
-                        }
-                        else {
-                            index = name.indexOf(',', separatorIndex);
-                            if (index == -1) {
-                                index = name.length();
-                            }
-                            value = name.substring(separatorIndex, index);
-                            ++index;
-                        }
-                        properties.put(key, value);
-                    }
-                }
-                final String upperMethodName = LOWER_CAMEL.to(UPPER_CAMEL, method.getName());
-                final List<String> keyNames = keyNameBuilder.build();
-
-                cacheBuilder.put(method, CacheBuilder.newBuilder()
-                        .ticker(ticker)
-                        .expireAfterAccess(15, TimeUnit.MINUTES)
-                        .removalListener(new UnexportRemovalListener())
-                        .build(new CacheLoader<List<Optional<String>>, Object>()
-                        {
-                            @Override
-                            public Object load(List<Optional<String>> key)
-                                    throws Exception
-                            {
-                                Object stat = constructor.newInstance();
-
-                                ObjectNameBuilder objectNameBuilder = new ObjectNameBuilder(packageName);
-                                for (Entry<String, String> entry : properties.entrySet()) {
-                                    objectNameBuilder = objectNameBuilder.withProperty(entry.getKey(), entry.getValue());
-                                }
-                                objectNameBuilder = objectNameBuilder.withProperty("name", upperMethodName);
-                                for (int i = 0; i < keyNames.size(); ++i) {
-                                    if (key.get(i).isPresent()) {
-                                        objectNameBuilder = objectNameBuilder.withProperty(keyNames.get(i), key.get(i).get());
-                                    }
-                                }
-                                String objectName = objectNameBuilder.build();
-                                reportExporter.export(objectName, stat);
-                                objectNameMap.put(stat, objectName);
-                                return stat;
-                            }
-                        })
-                );
+            Builder<Method, PerMethodCache> cacheBuilder = ImmutableMap.builder();
+            for (Method method : aClass.getMethods()) {
+                cacheBuilder.put(method, new PerMethodCache(aClass, method, name));
             }
             cacheMap = cacheBuilder.build();
         }
@@ -222,13 +117,154 @@ public class ReportCollectionFactory
             return cacheMap.get(method).get(argBuilder.build());
         }
 
+    }
+
+    private class PerMethodCache {
+        private final LoadingCache<List<Optional<String>>, Object> loadingCache;
+        @GuardedBy("registeredMap")
+        private final Map<List<Optional<String>>, Object> registeredMap = new HashMap<>();
+        @GuardedBy("registeredMap")
+        private final Set<Object> reinsertedSet = new HashSet<>();
+        @GuardedBy("registeredMap")
+        private final Map<Object, String> objectNameMap = new HashMap<>();
+
+        <T> PerMethodCache(Class<T> aClass, Method method, String name)
+        {
+            final Constructor<?> constructor;
+            try {
+                constructor = method.getReturnType().getConstructor();
+            }
+            catch (NoSuchMethodException e) {
+                throw new RuntimeException(methodName(method) + " return type " + method.getReturnType().getSimpleName()
+                        + " has no public no-arg constructor");
+            }
+
+            ImmutableList.Builder<String> keyNameBuilder = ImmutableList.builder();
+            int argPosition = 0;
+            for (Annotation[] annotations : method.getParameterAnnotations()) {
+                ++argPosition;
+                boolean found = false;
+                for (Annotation annotation : annotations) {
+                    if (Key.class.isAssignableFrom(annotation.annotationType())) {
+                        keyNameBuilder.add(((Key)annotation).value());
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    throw new RuntimeException(methodName(method) + " parameter " + argPosition
+                            + " has no @com.proofpoint.reporting.Key annotation");
+                }
+            }
+            final String packageName;
+            final Map<String, String> properties = new LinkedHashMap<>();
+            if (name == null) {
+                packageName = aClass.getPackage().getName();
+                properties.put("type", aClass.getSimpleName());
+            }
+            else {
+                ObjectName objectName;
+                try {
+                    objectName = ObjectName.getInstance(name);
+                }
+                catch (MalformedObjectNameException e) {
+                    throw propagate(e);
+                }
+                packageName = objectName.getDomain();
+                int index = packageName.length();
+                if (name.charAt(index++) != ':') {
+                    throw new RuntimeException("Unable to parse ObjectName " + name);
+                }
+                while (index < name.length()) {
+                    int separatorIndex = name.indexOf('=', index);
+                    String key = name.substring(index, separatorIndex);
+                    String value;
+                    if (name.charAt(++separatorIndex) == '\"') {
+                        StringBuilder sb = new StringBuilder();
+                        char c;
+                        while ((c = name.charAt(++separatorIndex)) != '\"') {
+                            if (c == '\\') {
+                                c = name.charAt(++separatorIndex);
+                            }
+                            sb.append(c);
+                        }
+                        if (name.charAt(++separatorIndex) != ',') {
+                            throw new RuntimeException("Unable to parse ObjectName " + name);
+                        }
+                        value = sb.toString();
+                        index = separatorIndex + 1;
+                    }
+                    else {
+                        index = name.indexOf(',', separatorIndex);
+                        if (index == -1) {
+                            index = name.length();
+                        }
+                        value = name.substring(separatorIndex, index);
+                        ++index;
+                    }
+                    properties.put(key, value);
+                }
+            }
+            final String upperMethodName = LOWER_CAMEL.to(UPPER_CAMEL, method.getName());
+            final List<String> keyNames = keyNameBuilder.build();
+            loadingCache = CacheBuilder.newBuilder()
+                    .ticker(ticker)
+                    .expireAfterAccess(15, TimeUnit.MINUTES)
+                    .removalListener(new UnexportRemovalListener())
+                    .build(new CacheLoader<List<Optional<String>>, Object>()
+                    {
+                        @Override
+                        public Object load(List<Optional<String>> key)
+                                throws Exception
+                        {
+                            Object stat = constructor.newInstance();
+
+                            ObjectNameBuilder objectNameBuilder = new ObjectNameBuilder(packageName);
+                            for (Entry<String, String> entry : properties.entrySet()) {
+                                objectNameBuilder = objectNameBuilder.withProperty(entry.getKey(), entry.getValue());
+                            }
+                            objectNameBuilder = objectNameBuilder.withProperty("name", upperMethodName);
+                            for (int i = 0; i < keyNames.size(); ++i) {
+                                if (key.get(i).isPresent()) {
+                                    objectNameBuilder = objectNameBuilder.withProperty(keyNames.get(i), key.get(i).get());
+                                }
+                            }
+                            String objectName = objectNameBuilder.build();
+
+                            synchronized (registeredMap) {
+                                Object existingStat = registeredMap.get(key);
+                                if (existingStat != null) {
+                                    reinsertedSet.add(existingStat);
+                                    return existingStat;
+                                }
+                                registeredMap.put(key, stat);
+                                reportExporter.export(objectName, stat);
+                                objectNameMap.put(stat, objectName);
+                            }
+                            return stat;
+                        }
+                    });
+        }
+
+        Object get(List<Optional<String>> key)
+                throws ExecutionException
+        {
+            return loadingCache.get(key);
+        }
+
         private class UnexportRemovalListener implements RemovalListener<List<Optional<String>>, Object>
         {
             @Override
             public void onRemoval(RemovalNotification<List<Optional<String>>, Object> notification)
             {
-                String objectName = objectNameMap.remove(notification.getValue());
-                reportExporter.unexport(objectName);
+                synchronized (registeredMap) {
+                    if (reinsertedSet.remove(notification.getValue())) {
+                        return;
+                    }
+                    String objectName = objectNameMap.remove(notification.getValue());
+                    reportExporter.unexport(objectName);
+                    registeredMap.remove(notification.getKey());
+                }
             }
         }
     }
