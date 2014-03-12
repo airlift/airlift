77a9c6525fc10e3a15c837bf5d494e65e314bf94
Dain Sundstrom <dain@iq80.com>
2014-03-12 13:48:18 -0700

    Add back support for legacy http-client.threads config

--- http-client/src/test/java/io/airlift/http/client/jetty/TestJettyIoPoolConfig.java
+++ http-client/src/test/java/io/airlift/http/client/jetty/TestJettyIoPoolConfig.java
@@ -30,4 +30,18 @@ public class TestJettyIoPoolConfig
 
         ConfigAssertions.assertFullMapping(properties, expected);
     }
+
+    @Test
+    public void testDeprecatedProperties()
+    {
+        Map<String, String> currentProperties = new ImmutableMap.Builder<String, String>()
+                .put("http-client.max-threads", "111")
+                .build();
+
+        Map<String, String> oldProperties = new ImmutableMap.Builder<String, String>()
+                .put("http-client.threads", "111")
+                .build();
+
+        ConfigAssertions.assertDeprecatedEquivalence(JettyIoPoolConfig.class, currentProperties, oldProperties);
+    }
 }
