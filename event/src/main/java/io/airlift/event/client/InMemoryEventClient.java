74ddd7f981b561ef325b7847fd911d32a0734a16
Christopher Berner <cberner@fb.com>
2014-06-02 14:47:53 -0700

    Add abstract event client class

--- event/src/main/java/io/airlift/event/client/InMemoryEventClient.java
+++ event/src/main/java/io/airlift/event/client/InMemoryEventClient.java
@@ -15,62 +15,24 @@
  */
 package io.airlift.event.client;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.util.concurrent.CheckedFuture;
-import com.google.common.util.concurrent.Futures;
 
 import java.io.IOException;
-import java.net.URI;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
 
-public class InMemoryEventClient implements EventClient
+public class InMemoryEventClient
+        extends AbstractEventClient
 {
-    private final List<Object> events = newArrayList();
+    private final List<Object> events = new ArrayList<>();
 
     @Override
-    public <T> CheckedFuture<Void, RuntimeException> post(T... events)
-            throws IllegalArgumentException
+    protected <T> void postEvent(T event)
+            throws IOException
     {
-        return post(Arrays.asList(events));
-    }
-
-    @Override
-    public <T> CheckedFuture<Void, RuntimeException> post(Iterable<T> events)
-            throws IllegalArgumentException
-    {
-        Preconditions.checkNotNull(events, "event is null");
-        for (T event : events) {
-            Preconditions.checkNotNull(event, "event is null");
-            this.events.add(event);
-        }
-        return Futures.immediateCheckedFuture(null);
-    }
-
-    @Override
-    public <T> CheckedFuture<Void, RuntimeException> post(EventGenerator<T> eventGenerator)
-            throws IllegalArgumentException
-    {
-        Preconditions.checkNotNull(eventGenerator, "eventGenerator is null");
-        try {
-            eventGenerator.generate(new EventPoster<T>()
-            {
-                @Override
-                public void post(T event)
-                {
-                    Preconditions.checkNotNull(event, "event is null");
-                    InMemoryEventClient.this.events.add(event);
-                }
-            });
-        }
-        catch (IOException e) {
-            return Futures.<Void, RuntimeException>immediateFailedCheckedFuture(new EventSubmissionFailedException("event", "general", ImmutableMap.of(URI.create("in-memory://"), e)));
-        }
-        return Futures.immediateCheckedFuture(null);
+        events.add(event);
     }
 
     public List<Object> getEvents()
