62c2e894f45ef155b81cf5a301d9c8828abbcbf9
Dain Sundstrom <dain@iq80.com>
2014-09-16 17:22:45 -0700

    Add more closeable utility methods

--- testing/src/main/java/io/airlift/testing/Closeables.java
+++ testing/src/main/java/io/airlift/testing/Closeables.java
@@ -1,20 +1,109 @@
 package io.airlift.testing;
 
-import javax.annotation.Nullable;
-
 import java.io.Closeable;
 import java.io.IOException;
 
-public class Closeables
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public final class Closeables
 {
-    public static void closeQuietly(@Nullable Closeable closeable)
+    private Closeables()
     {
-        try {
-            if (closeable != null) {
-                closeable.close();
+    }
+
+    public static void closeQuietly(Closeable... closeables)
+    {
+        if (closeables == null) {
+            return;
+        }
+        for (Closeable closeable : closeables) {
+            try {
+                if (closeable != null) {
+                    closeable.close();
+                }
+            }
+            catch (IOException | RuntimeException ignored) {
             }
         }
-        catch (IOException ignored) {
+    }
+
+    public static void closeAll(Closeable... closeables)
+            throws IOException
+    {
+        if (closeables == null) {
+            return;
         }
+        IOException rootCause = null;
+        for (Closeable closeable : closeables) {
+            try {
+                if (closeable != null) {
+                    closeable.close();
+                }
+            }
+            catch (IOException e) {
+                if (rootCause == null) {
+                    rootCause = e;
+                }
+                else {
+                    rootCause.addSuppressed(e);
+                }
+            }
+            catch (Throwable e) {
+                if (rootCause == null) {
+                    rootCause = new IOException(e);
+                }
+                else {
+                    rootCause.addSuppressed(e);
+                }
+            }
+        }
+        if (rootCause != null) {
+            throw rootCause;
+        }
+    }
+
+    public static void closeAllRuntimeException(Closeable... closeables)
+    {
+        if (closeables == null) {
+            return;
+        }
+        RuntimeException rootCause = null;
+        for (Closeable closeable : closeables) {
+            try {
+                if (closeable != null) {
+                    closeable.close();
+                }
+            }
+            catch (Throwable e) {
+                if (rootCause == null) {
+                    rootCause = new RuntimeException(e);
+                }
+                else {
+                    rootCause.addSuppressed(e);
+                }
+            }
+        }
+        if (rootCause != null) {
+            throw rootCause;
+        }
+    }
+
+    public static <T extends Throwable> T closeAllSuppress(T rootCause, Closeable... closeables)
+    {
+        checkNotNull(rootCause, "rootCause is null");
+        if (closeables == null) {
+            return rootCause;
+        }
+        for (Closeable closeable : closeables) {
+            try {
+                if (closeable != null) {
+                    closeable.close();
+                }
+            }
+            catch (Throwable e) {
+                rootCause.addSuppressed(e);
+            }
+        }
+        return rootCause;
     }
 }
