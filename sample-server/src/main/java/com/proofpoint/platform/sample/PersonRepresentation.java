proofpoint/platform

Make AutoValue available and use in sample server

sample-server/src/main/java/com/proofpoint/platform/sample/PersonRepresentation.java
@@ -17,38 +17,34 @@ package com.proofpoint.platform.sample;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Objects;
+import com.google.auto.value.AutoValue;
 
+import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
-public class PersonRepresentation
+import static com.proofpoint.platform.sample.Person.person;
+
+@AutoValue
+public abstract class PersonRepresentation
 {
+    @Nullable
     @NotNull(message = "is missing")
     @Pattern(regexp = "[^@]+@[^@]+", message = "is malformed")
-    private String email;
+    abstract String getEmail();
 
+    @Nullable
     @NotNull(message = "is missing")
-    private String name;
+    abstract String getName();
 
     @JsonCreator
-    public PersonRepresentation(@JsonProperty("email") String email, @JsonProperty("name") String name)
+    public static PersonRepresentation personRepresentation(@JsonProperty("email") String email, @JsonProperty("name") String name)
     {
-        this.email = email;
-        this.name = name;
+        return new AutoValue_PersonRepresentation(email, name);
     }
 
     public Person toPerson()
     {
-        return new Person(email, name);
-    }
-
-    @Override
-    public String toString()
-    {
-        return Objects.toStringHelper(this)
-                .add("email", email)
-                .add("name", name)
-                .toString();
+        return person(getEmail(), getName());
     }
 }

sample-server/src/test/java/com/proofpoint/platform/sample/TestPerson.java
@@ -17,6 +17,7 @@ package com.proofpoint.platform.sample;
 
 import org.testng.annotations.Test;
 
+import static com.proofpoint.platform.sample.Person.person;
 import static com.proofpoint.testing.EquivalenceTester.equivalenceTester;
 
 public class TestPerson
@@ -25,10 +26,10 @@ public class TestPerson
     public void testEquivalence()
     {
         equivalenceTester()
-                .addEquivalentGroup(new Person("foo@example.com", "Mr Foo"), new Person("foo@example.com", "Mr Foo"))
-                .addEquivalentGroup(new Person("bar@example.com", "Mr Bar"), new Person("bar@example.com", "Mr Bar"))
-                .addEquivalentGroup(new Person("foo@example.com", "Mr Bar"), new Person("foo@example.com", "Mr Bar"))
-                .addEquivalentGroup(new Person("bar@example.com", "Mr Foo"), new Person("bar@example.com", "Mr Foo"))
+                .addEquivalentGroup(person("foo@example.com", "Mr Foo"), person("foo@example.com", "Mr Foo"))
+                .addEquivalentGroup(person("bar@example.com", "Mr Bar"), person("bar@example.com", "Mr Bar"))
+                .addEquivalentGroup(person("foo@example.com", "Mr Bar"), person("foo@example.com", "Mr Bar"))
+                .addEquivalentGroup(person("bar@example.com", "Mr Foo"), person("bar@example.com", "Mr Foo"))
                 .check();
     }
 }

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonRepresentation.java
@@ -27,6 +27,7 @@ import java.util.Map;
 
 import static com.proofpoint.json.JsonCodec.jsonCodec;
 import static com.proofpoint.json.testing.JsonTester.decodeJson;
+import static com.proofpoint.platform.sample.Person.person;
 import static com.proofpoint.testing.ValidationAssertions.assertFailsValidation;
 import static com.proofpoint.testing.ValidationAssertions.assertValidates;
 import static org.testng.Assert.assertEquals;
@@ -47,7 +48,7 @@ public class TestPersonRepresentation
     public void testJsonDecode()
     {
         PersonRepresentation personRepresentation = assertValidates(decodeJson(codec, jsonStructure));
-        assertEquals(personRepresentation.toPerson(), new Person("foo@example.com", "Mr Foo"));
+        assertEquals(personRepresentation.toPerson(), person("foo@example.com", "Mr Foo"));
     }
 
     @Test

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonWithSelf.java
@@ -21,6 +21,7 @@ import org.testng.annotations.Test;
 import java.net.URI;
 
 import static com.proofpoint.json.testing.JsonTester.assertJsonEncode;
+import static com.proofpoint.platform.sample.Person.person;
 import static com.proofpoint.platform.sample.PersonWithSelf.createPersonWithSelf;
 import static com.proofpoint.testing.EquivalenceTester.equivalenceTester;
 
@@ -29,10 +30,10 @@ public class TestPersonWithSelf
     @Test
     public void testEquivalence()
     {
-        Person fooPerson = new Person("foo@example.com", "Mr Foo");
-        Person fooPerson2 = new Person("foo@example.com", "Mr Foo");
-        Person barPerson = new Person("bar@example.com", "Mr Bar");
-        Person barPerson2 = new Person("bar@example.com", "Mr Bar");
+        Person fooPerson = person("foo@example.com", "Mr Foo");
+        Person fooPerson2 = person("foo@example.com", "Mr Foo");
+        Person barPerson = person("bar@example.com", "Mr Bar");
+        Person barPerson2 = person("bar@example.com", "Mr Bar");
         equivalenceTester()
                 .addEquivalentGroup(createPersonWithSelf(fooPerson, URI.create("http://example.com")), createPersonWithSelf(fooPerson2, URI.create("http://example.com")))
                 .addEquivalentGroup(createPersonWithSelf(fooPerson, URI.create("https://example.com")), createPersonWithSelf(fooPerson2, URI.create("https://example.com")))
@@ -44,7 +45,7 @@ public class TestPersonWithSelf
     @Test
     public void testJsonEncode()
     {
-        assertJsonEncode(createPersonWithSelf(new Person("alice@example.com", "Alice"), URI.create("http://example.com/foo")),
+        assertJsonEncode(createPersonWithSelf(person("alice@example.com", "Alice"), URI.create("http://example.com/foo")),
                 ImmutableMap.of(
                         "self", "http://example.com/foo",
                         "name", "Alice",

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonsResource.java
@@ -25,6 +25,7 @@ import java.net.URI;
 import java.util.Collection;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static com.proofpoint.platform.sample.Person.person;
 import static com.proofpoint.platform.sample.PersonWithSelf.createPersonWithSelf;
 import static com.proofpoint.testing.Assertions.assertInstanceOf;
 import static org.testng.Assert.assertEquals;
@@ -53,15 +54,15 @@ public class TestPersonsResource
     @Test
     public void testListAll()
     {
-        store.put("foo", new Person("foo@example.com", "Mr Foo"));
-        store.put("bar", new Person("bar@example.com", "Mr Bar"));
+        store.put("foo", person("foo@example.com", "Mr Foo"));
+        store.put("bar", person("bar@example.com", "Mr Bar"));
 
         Response response = resource.listAll(MockUriInfo.from(URI.create("http://localhost/v1/person")));
         assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
         assertInstanceOf(response.getEntity(), Collection.class);
         assertEquals((Collection<?>) response.getEntity(), newArrayList(
-                createPersonWithSelf(new Person("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/foo")),
-                createPersonWithSelf(new Person("bar@example.com", "Mr Bar"), URI.create("http://localhost/v1/person/bar"))
+                createPersonWithSelf(person("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/foo")),
+                createPersonWithSelf(person("bar@example.com", "Mr Bar"), URI.create("http://localhost/v1/person/bar"))
         ));
     }
 }

sample-server/src/test/java/com/proofpoint/platform/sample/TestServer.java
@@ -59,6 +59,7 @@ import static com.proofpoint.http.client.StatusResponseHandler.createStatusRespo
 import static com.proofpoint.jaxrs.JaxrsModule.explicitJaxrsModule;
 import static com.proofpoint.json.JsonCodec.listJsonCodec;
 import static com.proofpoint.json.JsonCodec.mapJsonCodec;
+import static com.proofpoint.platform.sample.Person.person;
 import static com.proofpoint.platform.sample.PersonEvent.personAdded;
 import static com.proofpoint.platform.sample.PersonEvent.personRemoved;
 import static com.proofpoint.testing.Assertions.assertEqualsIgnoreOrder;
@@ -139,8 +140,8 @@ public class TestServer
     public void testGetAll()
             throws IOException, ExecutionException, InterruptedException
     {
-        store.put("bar", new Person("bar@example.com", "Mr Bar"));
-        store.put("foo", new Person("foo@example.com", "Mr Foo"));
+        store.put("bar", person("bar@example.com", "Mr Bar"));
+        store.put("foo", person("foo@example.com", "Mr Foo"));
 
         List<Object> expected = new ArrayList<>();
         expected.add(ImmutableMap.of("self", uriFor("/v1/person/foo").toString(), "name", "Mr Foo", "email", "foo@example.com"));
@@ -156,7 +157,7 @@ public class TestServer
     public void testGetSingle()
             throws IOException, ExecutionException, InterruptedException
     {
-        store.put("foo", new Person("foo@example.com", "Mr Foo"));
+        store.put("foo", person("foo@example.com", "Mr Foo"));
 
         URI requestUri = uriFor("/v1/person/foo");
 
@@ -186,10 +187,10 @@ public class TestServer
 
         assertEquals(response.getStatusCode(), javax.ws.rs.core.Response.Status.CREATED.getStatusCode());
 
-        assertEquals(store.get("foo"), new Person("foo@example.com", "Mr Foo"));
+        assertEquals(store.get("foo"), person("foo@example.com", "Mr Foo"));
 
         assertEquals(eventClient.getEvents(), ImmutableList.of(
-                personAdded("foo", new Person("foo@example.com", "Mr Foo"))
+                personAdded("foo", person("foo@example.com", "Mr Foo"))
         ));
     }
 
@@ -197,7 +198,7 @@ public class TestServer
     public void testDelete()
             throws IOException, ExecutionException, InterruptedException
     {
-        store.put("foo", new Person("foo@example.com", "Mr Foo"));
+        store.put("foo", person("foo@example.com", "Mr Foo"));
 
         StatusResponse response = client.execute(
                 prepareDelete()
@@ -211,8 +212,8 @@ public class TestServer
         assertNull(store.get("foo"));
 
         assertEquals(eventClient.getEvents(), ImmutableList.of(
-                personAdded("foo", new Person("foo@example.com", "Mr Foo")),
-                personRemoved("foo", new Person("foo@example.com", "Mr Foo"))
+                personAdded("foo", person("foo@example.com", "Mr Foo")),
+                personRemoved("foo", person("foo@example.com", "Mr Foo"))
         ));
     }
 

airlift/airlift

sample-server/src/main/java/com/proofpoint/platform/sample/PersonRepresentation.java
@@ -17,38 +17,34 @@ package com.proofpoint.platform.sample;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Objects;
+import com.google.auto.value.AutoValue;
 
+import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
-public class PersonRepresentation
+import static com.proofpoint.platform.sample.Person.createPerson;
+
+@AutoValue
+public abstract class PersonRepresentation
 {
+    @Nullable
     @NotNull(message = "is missing")
     @Pattern(regexp = "[^@]+@[^@]+", message = "is malformed")
-    private String email;
+    abstract String getEmail();
 
+    @Nullable
     @NotNull(message = "is missing")
-    private String name;
+    abstract String getName();
 
     @JsonCreator
-    public PersonRepresentation(@JsonProperty("email") String email, @JsonProperty("name") String name)
+    public static PersonRepresentation createPersonRepresentation(@JsonProperty("email") String email, @JsonProperty("name") String name)
     {
-        this.email = email;
-        this.name = name;
+        return new AutoValue_PersonRepresentation(email, name);
     }
 
     public Person toPerson()
     {
-        return new Person(email, name);
-    }
-
-    @Override
-    public String toString()
-    {
-        return Objects.toStringHelper(this)
-                .add("email", email)
-                .add("name", name)
-                .toString();
+        return createPerson(getEmail(), getName());
     }
 }

sample-server/src/test/java/com/proofpoint/platform/sample/TestPerson.java
@@ -17,6 +17,7 @@ package com.proofpoint.platform.sample;
 
 import org.testng.annotations.Test;
 
+import static com.proofpoint.platform.sample.Person.createPerson;
 import static com.proofpoint.testing.EquivalenceTester.equivalenceTester;
 
 public class TestPerson
@@ -25,10 +26,10 @@ public class TestPerson
     public void testEquivalence()
     {
         equivalenceTester()
-                .addEquivalentGroup(new Person("foo@example.com", "Mr Foo"), new Person("foo@example.com", "Mr Foo"))
-                .addEquivalentGroup(new Person("bar@example.com", "Mr Bar"), new Person("bar@example.com", "Mr Bar"))
-                .addEquivalentGroup(new Person("foo@example.com", "Mr Bar"), new Person("foo@example.com", "Mr Bar"))
-                .addEquivalentGroup(new Person("bar@example.com", "Mr Foo"), new Person("bar@example.com", "Mr Foo"))
+                .addEquivalentGroup(createPerson("foo@example.com", "Mr Foo"), createPerson("foo@example.com", "Mr Foo"))
+                .addEquivalentGroup(createPerson("bar@example.com", "Mr Bar"), createPerson("bar@example.com", "Mr Bar"))
+                .addEquivalentGroup(createPerson("foo@example.com", "Mr Bar"), createPerson("foo@example.com", "Mr Bar"))
+                .addEquivalentGroup(createPerson("bar@example.com", "Mr Foo"), createPerson("bar@example.com", "Mr Foo"))
                 .check();
     }
 }

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonRepresentation.java
@@ -27,6 +27,7 @@ import java.util.Map;
 
 import static com.proofpoint.json.JsonCodec.jsonCodec;
 import static com.proofpoint.json.testing.JsonTester.decodeJson;
+import static com.proofpoint.platform.sample.Person.createPerson;
 import static com.proofpoint.testing.ValidationAssertions.assertFailsValidation;
 import static com.proofpoint.testing.ValidationAssertions.assertValidates;
 import static org.testng.Assert.assertEquals;
@@ -47,7 +48,7 @@ public class TestPersonRepresentation
     public void testJsonDecode()
     {
         PersonRepresentation personRepresentation = assertValidates(decodeJson(codec, jsonStructure));
-        assertEquals(personRepresentation.toPerson(), new Person("foo@example.com", "Mr Foo"));
+        assertEquals(personRepresentation.toPerson(), createPerson("foo@example.com", "Mr Foo"));
     }
 
     @Test

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonWithSelf.java
@@ -21,6 +21,7 @@ import org.testng.annotations.Test;
 import java.net.URI;
 
 import static com.proofpoint.json.testing.JsonTester.assertJsonEncode;
+import static com.proofpoint.platform.sample.Person.createPerson;
 import static com.proofpoint.platform.sample.PersonWithSelf.createPersonWithSelf;
 import static com.proofpoint.testing.EquivalenceTester.equivalenceTester;
 
@@ -29,10 +30,10 @@ public class TestPersonWithSelf
     @Test
     public void testEquivalence()
     {
-        Person fooPerson = new Person("foo@example.com", "Mr Foo");
-        Person fooPerson2 = new Person("foo@example.com", "Mr Foo");
-        Person barPerson = new Person("bar@example.com", "Mr Bar");
-        Person barPerson2 = new Person("bar@example.com", "Mr Bar");
+        Person fooPerson = createPerson("foo@example.com", "Mr Foo");
+        Person fooPerson2 = createPerson("foo@example.com", "Mr Foo");
+        Person barPerson = createPerson("bar@example.com", "Mr Bar");
+        Person barPerson2 = createPerson("bar@example.com", "Mr Bar");
         equivalenceTester()
                 .addEquivalentGroup(createPersonWithSelf(fooPerson, URI.create("http://example.com")), createPersonWithSelf(fooPerson2, URI.create("http://example.com")))
                 .addEquivalentGroup(createPersonWithSelf(fooPerson, URI.create("https://example.com")), createPersonWithSelf(fooPerson2, URI.create("https://example.com")))
@@ -44,7 +45,7 @@ public class TestPersonWithSelf
     @Test
     public void testJsonEncode()
     {
-        assertJsonEncode(createPersonWithSelf(new Person("alice@example.com", "Alice"), URI.create("http://example.com/foo")),
+        assertJsonEncode(createPersonWithSelf(createPerson("alice@example.com", "Alice"), URI.create("http://example.com/foo")),
                 ImmutableMap.of(
                         "self", "http://example.com/foo",
                         "name", "Alice",

sample-server/src/test/java/com/proofpoint/platform/sample/TestPersonsResource.java
@@ -25,6 +25,7 @@ import java.net.URI;
 import java.util.Collection;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static com.proofpoint.platform.sample.Person.createPerson;
 import static com.proofpoint.platform.sample.PersonWithSelf.createPersonWithSelf;
 import static com.proofpoint.testing.Assertions.assertInstanceOf;
 import static org.testng.Assert.assertEquals;
@@ -53,15 +54,15 @@ public class TestPersonsResource
     @Test
     public void testListAll()
     {
-        store.put("foo", new Person("foo@example.com", "Mr Foo"));
-        store.put("bar", new Person("bar@example.com", "Mr Bar"));
+        store.put("foo", createPerson("foo@example.com", "Mr Foo"));
+        store.put("bar", createPerson("bar@example.com", "Mr Bar"));
 
         Response response = resource.listAll(MockUriInfo.from(URI.create("http://localhost/v1/person")));
         assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
         assertInstanceOf(response.getEntity(), Collection.class);
         assertEquals((Collection<?>) response.getEntity(), newArrayList(
-                createPersonWithSelf(new Person("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/foo")),
-                createPersonWithSelf(new Person("bar@example.com", "Mr Bar"), URI.create("http://localhost/v1/person/bar"))
+                createPersonWithSelf(createPerson("foo@example.com", "Mr Foo"), URI.create("http://localhost/v1/person/foo")),
+                createPersonWithSelf(createPerson("bar@example.com", "Mr Bar"), URI.create("http://localhost/v1/person/bar"))
         ));
     }
 }

sample-server/src/test/java/com/proofpoint/platform/sample/TestServer.java
@@ -59,6 +59,7 @@ import static com.proofpoint.http.client.StatusResponseHandler.createStatusRespo
 import static com.proofpoint.jaxrs.JaxrsModule.explicitJaxrsModule;
 import static com.proofpoint.json.JsonCodec.listJsonCodec;
 import static com.proofpoint.json.JsonCodec.mapJsonCodec;
+import static com.proofpoint.platform.sample.Person.createPerson;
 import static com.proofpoint.platform.sample.PersonEvent.personAdded;
 import static com.proofpoint.platform.sample.PersonEvent.personRemoved;
 import static com.proofpoint.testing.Assertions.assertEqualsIgnoreOrder;
@@ -139,8 +140,8 @@ public class TestServer
     public void testGetAll()
             throws IOException, ExecutionException, InterruptedException
     {
-        store.put("bar", new Person("bar@example.com", "Mr Bar"));
-        store.put("foo", new Person("foo@example.com", "Mr Foo"));
+        store.put("bar", createPerson("bar@example.com", "Mr Bar"));
+        store.put("foo", createPerson("foo@example.com", "Mr Foo"));
 
         List<Object> expected = new ArrayList<>();
         expected.add(ImmutableMap.of("self", uriFor("/v1/person/foo").toString(), "name", "Mr Foo", "email", "foo@example.com"));
@@ -156,7 +157,7 @@ public class TestServer
     public void testGetSingle()
             throws IOException, ExecutionException, InterruptedException
     {
-        store.put("foo", new Person("foo@example.com", "Mr Foo"));
+        store.put("foo", createPerson("foo@example.com", "Mr Foo"));
 
         URI requestUri = uriFor("/v1/person/foo");
 
@@ -186,10 +187,10 @@ public class TestServer
 
         assertEquals(response.getStatusCode(), javax.ws.rs.core.Response.Status.CREATED.getStatusCode());
 
-        assertEquals(store.get("foo"), new Person("foo@example.com", "Mr Foo"));
+        assertEquals(store.get("foo"), createPerson("foo@example.com", "Mr Foo"));
 
         assertEquals(eventClient.getEvents(), ImmutableList.of(
-                personAdded("foo", new Person("foo@example.com", "Mr Foo"))
+                personAdded("foo", createPerson("foo@example.com", "Mr Foo"))
         ));
     }
 
@@ -197,7 +198,7 @@ public class TestServer
     public void testDelete()
             throws IOException, ExecutionException, InterruptedException
     {
-        store.put("foo", new Person("foo@example.com", "Mr Foo"));
+        store.put("foo", createPerson("foo@example.com", "Mr Foo"));
 
         StatusResponse response = client.execute(
                 prepareDelete()
@@ -211,8 +212,8 @@ public class TestServer
         assertNull(store.get("foo"));
 
         assertEquals(eventClient.getEvents(), ImmutableList.of(
-                personAdded("foo", new Person("foo@example.com", "Mr Foo")),
-                personRemoved("foo", new Person("foo@example.com", "Mr Foo"))
+                personAdded("foo", createPerson("foo@example.com", "Mr Foo")),
+                personRemoved("foo", createPerson("foo@example.com", "Mr Foo"))
         ));
     }
 