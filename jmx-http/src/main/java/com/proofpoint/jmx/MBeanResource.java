05a78f7383b88ad790920a55bc70f4fe2bb7b087
Dain Sundstrom <dain@iq80.com>
2014-11-06 00:05:39 -0800

    Add REST endpoint to get MBean attribute value

--- jmx-http/src/main/java/com/proofpoint/jmx/MBeanResource.java
+++ jmx-http/src/main/java/com/proofpoint/jmx/MBeanResource.java
@@ -65,4 +65,14 @@ public class MBeanResource
         Preconditions.checkNotNull(objectName, "objectName is null");
         return new MBeanRepresentation(mbeanServer, objectName, objectMapper);
     }
+
+    @GET
+    @Path("{objectName}/{attributeName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object getMBean(@PathParam("objectName") ObjectName objectName, @PathParam("attributeName") String attributeName)
+            throws JMException
+    {
+        Preconditions.checkNotNull(objectName, "objectName is null");
+        return mbeanServer.getAttribute(objectName, attributeName);
+    }
 }
