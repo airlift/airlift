7b810bf7f896e2af9cca6cd9e35be9e112dfc2e4
John Myers <jgmyers@proofpoint.com>
2015-05-15 10:34:52 -0700

    JettyIoPool cleanup

--- http-client/src/main/java/io/airlift/http/client/jetty/JettyIoPool.java
+++ http-client/src/main/java/io/airlift/http/client/jetty/JettyIoPool.java
@@ -3,19 +3,16 @@ package io.airlift.http.client.jetty;
 import com.google.common.base.Throwables;
 import org.eclipse.jetty.io.ByteBufferPool;
 import org.eclipse.jetty.io.MappedByteBufferPool;
-import org.eclipse.jetty.util.component.AbstractLifeCycle;
 import org.eclipse.jetty.util.component.LifeCycle;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.util.thread.ScheduledExecutorScheduler;
 import org.eclipse.jetty.util.thread.Scheduler;
 
 import java.io.Closeable;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
-import static com.google.common.base.Preconditions.checkNotNull;
+import static java.lang.Thread.currentThread;
 
 public final class JettyIoPool
         implements Closeable
@@ -32,14 +29,14 @@ public final class JettyIoPool
             String baseName = "http-client-" + name;
 
             ThreadGroup threadGroup = new ThreadGroup(baseName);
-            QueuedThreadPool threadPool = new JettyThreadPool(threadGroup, config);
+            QueuedThreadPool threadPool = new QueuedThreadPool(config.getMaxThreads(), config.getMinThreads(), 60000, null, threadGroup);
             threadPool.setName(baseName);
             threadPool.setDaemon(true);
             threadPool.start();
             threadPool.setStopTimeout(2000);
             executor = threadPool;
 
-            scheduler = new JettyScheduler(threadGroup, baseName + "-scheduler");
+            scheduler = new ScheduledExecutorScheduler(baseName + "-scheduler", true, currentThread().getContextClassLoader(), threadGroup);
             scheduler.start();
 
             byteBufferPool = new MappedByteBufferPool();
@@ -98,87 +95,5 @@ public final class JettyIoPool
         return toStringHelper(this)
                 .add("name", name)
                 .toString();
-    }
-
-    // TODO: Jetty should have support for setting ThreadGroup
-    private static class JettyThreadPool
-            extends QueuedThreadPool
-    {
-        private final ThreadGroup threadGroup;
-
-        private JettyThreadPool(ThreadGroup threadGroup, JettyIoPoolConfig config)
-        {
-            super(config.getMaxThreads(), config.getMinThreads());
-            this.threadGroup = checkNotNull(threadGroup, "threadGroup is null");
-        }
-
-        @Override
-        protected Thread newThread(Runnable runnable)
-        {
-            return new Thread(threadGroup, runnable);
-        }
-    }
-
-    // TODO: Jetty should have support for setting ThreadGroup
-    // forked from org.eclipse.jetty.util.thread.ScheduledExecutorScheduler
-    private static class JettyScheduler
-            extends AbstractLifeCycle
-            implements Scheduler
-    {
-        private final String name;
-        private final ThreadGroup threadGroup;
-        private volatile ScheduledThreadPoolExecutor scheduler;
-
-        public JettyScheduler(ThreadGroup threadGroup, String name)
-        {
-            this.threadGroup = checkNotNull(threadGroup, "threadGroup is null");
-            this.name = checkNotNull(name, "name is null");
-        }
-
-        @Override
-        protected void doStart()
-                throws Exception
-        {
-            scheduler = new ScheduledThreadPoolExecutor(1, runnable -> {
-                Thread thread = new Thread(threadGroup, runnable);
-                thread.setName(name + "-" + thread.getId());
-                thread.setDaemon(true);
-                return thread;
-            });
-            scheduler.setRemoveOnCancelPolicy(true);
-            super.doStart();
-        }
-
-        @Override
-        protected void doStop()
-                throws Exception
-        {
-            scheduler.shutdownNow();
-            super.doStop();
-            scheduler = null;
-        }
-
-        @Override
-        public Task schedule(Runnable task, long delay, TimeUnit unit)
-        {
-            return new ScheduledFutureTask(scheduler.schedule(task, delay, unit));
-        }
-
-        private class ScheduledFutureTask
-                implements Task
-        {
-            private final ScheduledFuture<?> scheduledFuture;
-
-            public ScheduledFutureTask(ScheduledFuture<?> scheduledFuture)
-            {
-                this.scheduledFuture = checkNotNull(scheduledFuture, "scheduledFuture is null");
-            }
-
-            @Override
-            public boolean cancel()
-            {
-                return scheduledFuture.cancel(false);
-            }
-        }
     }
 }
