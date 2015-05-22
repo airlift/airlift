01828952da1c65d1f20f5897d01bd8b3968e0c65
John Myers <jgmyers@proofpoint.com>
2015-05-22 16:05:49 -0700

    Disable DHE ciphers

--- http-client/src/main/java/com/proofpoint/http/client/jetty/JettyHttpClient.java
+++ http-client/src/main/java/com/proofpoint/http/client/jetty/JettyHttpClient.java
@@ -93,13 +93,27 @@ import static java.util.concurrent.TimeUnit.NANOSECONDS;
 public class JettyHttpClient
         implements com.proofpoint.http.client.HttpClient
 {
-    private static final String[] DISABLED_CIPHERS = new String[] {
+    private static final String[] DISABLED_CIPHERS = {
             "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA",
             "TLS_ECDHE_RSA_WITH_RC4_128_SHA",
             "SSL_RSA_WITH_RC4_128_SHA",
             "TLS_ECDH_ECDSA_WITH_RC4_128_SHA",
             "TLS_ECDH_RSA_WITH_RC4_128_SHA",
-            "SSL_RSA_WITH_RC4_128_MD5"
+            "SSL_RSA_WITH_RC4_128_MD5",
+            "TLS_DHE_RSA_WITH_AES_256_CBC_SHA256",
+            "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256",
+            "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
+            "TLS_DHE_DSS_WITH_AES_256_CBC_SHA",
+            "TLS_DHE_RSA_WITH_AES_128_CBC_SHA256",
+            "TLS_DHE_DSS_WITH_AES_128_CBC_SHA256",
+            "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
+            "TLS_DHE_DSS_WITH_AES_128_CBC_SHA",
+            "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
+            "TLS_DHE_DSS_WITH_AES_256_GCM_SHA384",
+            "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256",
+            "TLS_DHE_DSS_WITH_AES_128_GCM_SHA256",
+            "SSL_DHE_RSA_WITH_3DES_EDE_CBC_SHA",
+            "SSL_DHE_DSS_WITH_3DES_EDE_CBC_SHA",
     };
 
     private static final AtomicLong nameCounter = new AtomicLong();
--- http-server/src/main/java/com/proofpoint/http/server/HttpServer.java
+++ http-server/src/main/java/com/proofpoint/http/server/HttpServer.java
@@ -66,13 +66,27 @@ import static java.lang.String.format;
 
 public class HttpServer
 {
-    private static final String[] DISABLED_CIPHERS = new String[] {
+    private static final String[] DISABLED_CIPHERS = {
             "TLS_ECDHE_ECDSA_WITH_RC4_128_SHA",
             "TLS_ECDHE_RSA_WITH_RC4_128_SHA",
             "SSL_RSA_WITH_RC4_128_SHA",
             "TLS_ECDH_ECDSA_WITH_RC4_128_SHA",
             "TLS_ECDH_RSA_WITH_RC4_128_SHA",
-            "SSL_RSA_WITH_RC4_128_MD5"
+            "SSL_RSA_WITH_RC4_128_MD5",
+            "TLS_DHE_RSA_WITH_AES_256_CBC_SHA256",
+            "TLS_DHE_DSS_WITH_AES_256_CBC_SHA256",
+            "TLS_DHE_RSA_WITH_AES_256_CBC_SHA",
+            "TLS_DHE_DSS_WITH_AES_256_CBC_SHA",
+            "TLS_DHE_RSA_WITH_AES_128_CBC_SHA256",
+            "TLS_DHE_DSS_WITH_AES_128_CBC_SHA256",
+            "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
+            "TLS_DHE_DSS_WITH_AES_128_CBC_SHA",
+            "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384",
+            "TLS_DHE_DSS_WITH_AES_256_GCM_SHA384",
+            "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256",
+            "TLS_DHE_DSS_WITH_AES_128_GCM_SHA256",
+            "SSL_DHE_RSA_WITH_3DES_EDE_CBC_SHA",
+            "SSL_DHE_DSS_WITH_3DES_EDE_CBC_SHA",
     };
 
     private final Server server;
