23d97ecbac6a49d1741c0220aba1a2cecf9b6d63
Dain Sundstrom <dain@iq80.com>
2016-03-28 16:31:26 -0700

    Cleanup BenchmarkQuantileDigest

--- stats/src/test/java/io/airlift/stats/BenchmarkQuantileDigest.java
+++ stats/src/test/java/io/airlift/stats/BenchmarkQuantileDigest.java
@@ -6,35 +6,33 @@ import io.airlift.units.Duration;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-public class BenchmarkQuantileDigest
+public final class BenchmarkQuantileDigest
 {
+    private BenchmarkQuantileDigest() {}
+
     public static void main(String[] args)
             throws Exception
     {
         Duration warmupTime = new Duration(3, TimeUnit.SECONDS);
         Duration benchmarkTime = new Duration(5, TimeUnit.SECONDS);
 
-        final QuantileDigest digest = new QuantileDigest(0.01, 0, new TestingTicker(), true);
-        final Random random = new Random();
+        QuantileDigest digest = new QuantileDigest(0.01, 0, new TestingTicker(), true);
+        Random random = new Random();
 
-        Benchmark.Results results = Benchmark.run(new Runnable() {
-            public void run()
-            {
-                digest.add(Math.abs(random.nextInt(100000)));
-            }
-        }, warmupTime, benchmarkTime);
+        Benchmark.Results results = Benchmark.run(() -> digest.add(Math.abs(random.nextInt(100000))), warmupTime, benchmarkTime);
 
         digest.validate();
 
-        System.out.println(String.format("Processed %s entries in %s ms. Insertion rate = %s entries/s (%.4fµs per operation)",
+        System.out.println(String.format(
+                "Processed %s entries in %s ms. Insertion rate = %s entries/s (%.4fµs per operation)",
                 results.getOperations(),
                 results.getTime().getValue(TimeUnit.MILLISECONDS),
                 results.getOperationsPerSecond(),
                 results.getTimePerOperation().getValue(TimeUnit.MICROSECONDS)));
 
-        System.out.println(String.format("Compressions: %s, %s entries/compression",
+        System.out.println(String.format(
+                "Compressions: %s, %s entries/compression",
                 digest.getCompressions(),
                 digest.getCount() / digest.getCompressions()));
     }
-
 }
