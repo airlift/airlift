63be25e5579affa30efd862ae3895f42d4de7ca2
David Phillips <david@acz.org>
2016-04-26 18:16:13 -0700

    Use default thread count for TestingHttpServerModule

--- http-server/src/main/java/io/airlift/http/server/testing/TestingHttpServerModule.java
+++ http-server/src/main/java/io/airlift/http/server/testing/TestingHttpServerModule.java
@@ -51,8 +51,7 @@ public class TestingHttpServerModule
     {
         binder.disableCircularProxies();
 
-        // Jetty scales required threads based on processor count, so pick a safe number
-        HttpServerConfig config = new HttpServerConfig().setMinThreads(1).setMaxThreads(20).setHttpPort(httpPort);
+        HttpServerConfig config = new HttpServerConfig().setHttpPort(httpPort);
 
         binder.bind(HttpServerConfig.class).toInstance(config);
         binder.bind(HttpServerInfo.class).in(Scopes.SINGLETON);
