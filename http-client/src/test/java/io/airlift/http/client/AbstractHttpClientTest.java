926fb051d79c928d18aeb3bd6f53ef108db6ae5e
David Phillips <david@acz.org>
2016-09-21 16:28:55 -0700

    Ignore socket backlog error for HTTP client tests
    Using a socket backlog to simulate a connection timeout is hack that
    doesn't always work reliably, so skip the test instead of failing.

--- http-client/src/test/java/io/airlift/http/client/AbstractHttpClientTest.java
+++ http-client/src/test/java/io/airlift/http/client/AbstractHttpClientTest.java
@@ -22,6 +22,7 @@ import org.eclipse.jetty.server.handler.gzip.GzipHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -1052,7 +1053,7 @@ public abstract class AbstractHttpClientTest
                 }
                 i++;
             }
-            fail(format("socket backlog is too large (%s connections accepted)", i));
+            throw new SkipException(format("socket backlog is too large (%s connections accepted)", i));
         }
 
         @Override
