proofpoint/platform

Mark @ConfigSecuritySensitive as appropriate.

dbpool/src/main/java/com/proofpoint/dbpool/H2EmbeddedDataSourceConfig.java
@@ -16,6 +16,7 @@
 package com.proofpoint.dbpool;
 
 import com.proofpoint.configuration.Config;
+import com.proofpoint.configuration.ConfigSecuritySensitive;
 
 /**
  * Configuration for (@see H2EmbeddedDataSource}.
@@ -82,6 +83,7 @@ public class H2EmbeddedDataSourceConfig extends ManagedDataSourceConfig<H2Embedd
     }
 
     @Config("db.file-password")
+    @ConfigSecuritySensitive
     public H2EmbeddedDataSourceConfig setFilePassword(String filePassword)
     {
         this.filePassword = filePassword;

http-server/src/main/java/com/proofpoint/http/server/HttpServerConfig.java
@@ -16,6 +16,7 @@
 package com.proofpoint.http.server;
 
 import com.proofpoint.configuration.Config;
+import com.proofpoint.configuration.ConfigSecuritySensitive;
 import com.proofpoint.configuration.LegacyConfig;
 import com.proofpoint.units.Duration;
 
@@ -119,6 +120,7 @@ public class HttpServerConfig
 
     @Config("http-server.https.keystore.key")
     @LegacyConfig({"jetty.https.keystore.password", "http-server.https.keystore.password"})
+    @ConfigSecuritySensitive
     public HttpServerConfig setKeystorePassword(String keystorePassword)
     {
         this.keystorePassword = keystorePassword;

jmx-http-rpc/src/main/java/com/proofpoint/jmx/http/rpc/JmxHttpRpcConfig.java
@@ -16,6 +16,7 @@
 package com.proofpoint.jmx.http.rpc;
 
 import com.proofpoint.configuration.Config;
+import com.proofpoint.configuration.ConfigSecuritySensitive;
 
 public class JmxHttpRpcConfig
 {
@@ -40,6 +41,7 @@ public class JmxHttpRpcConfig
     }
 
     @Config("jmx-http-rpc.password")
+    @ConfigSecuritySensitive
     public JmxHttpRpcConfig setPassword(String password)
     {
         this.password = password;

airlift/airlift

dbpool/src/main/java/io/airlift/dbpool/H2EmbeddedDataSourceConfig.java
@@ -16,6 +16,7 @@
 package io.airlift.dbpool;
 
 import io.airlift.configuration.Config;
+import io.airlift.configuration.ConfigSecuritySensitive;
 import io.airlift.configuration.ConfigDescription;
 
 import javax.validation.constraints.NotNull;
@@ -89,6 +90,7 @@ public class H2EmbeddedDataSourceConfig extends ManagedDataSourceConfig<H2Embedd
     }
 
     @Config("db.file-password")
+    @ConfigSecuritySensitive
     public H2EmbeddedDataSourceConfig setFilePassword(String filePassword)
     {
         this.filePassword = filePassword;

http-server/src/main/java/io/airlift/http/server/HttpServerConfig.java
@@ -18,6 +18,7 @@ package io.airlift.http.server;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import io.airlift.configuration.Config;
+import io.airlift.configuration.ConfigSecuritySensitive;
 import io.airlift.configuration.ConfigDescription;
 import io.airlift.configuration.DefunctConfig;
 import io.airlift.units.DataSize;
@@ -165,6 +166,7 @@ public class HttpServerConfig
     }
 
     @Config("http-server.https.keystore.key")
+    @ConfigSecuritySensitive
     public HttpServerConfig setKeystorePassword(String keystorePassword)
     {
         this.keystorePassword = keystorePassword;

jmx-http-rpc/src/main/java/io/airlift/jmx/http/rpc/JmxHttpRpcConfig.java
@@ -16,6 +16,7 @@
 package io.airlift.jmx.http.rpc;
 
 import io.airlift.configuration.Config;
+import io.airlift.configuration.ConfigSecuritySensitive;
 
 public class JmxHttpRpcConfig
 {
@@ -40,6 +41,7 @@ public class JmxHttpRpcConfig
     }
 
     @Config("jmx-http-rpc.password")
+    @ConfigSecuritySensitive
     public JmxHttpRpcConfig setPassword(String password)
     {
         this.password = password;