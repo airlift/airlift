9645c8bd8028606a899cce9f2e43a03121b33d26
Nezih Yigitbasi <nezihy@fb.com>
2017-12-11 09:28:45 -0800

    Rename variable in JettyHttpClient

--- http-client/src/main/java/io/airlift/http/client/jetty/JettyHttpClient.java
+++ http-client/src/main/java/io/airlift/http/client/jetty/JettyHttpClient.java
@@ -623,7 +623,7 @@ public class JettyHttpClient
     {
         long now = System.nanoTime();
         return getRequestListenersForDestination(destination).stream()
-                .map(request -> dumpRequest(now, request))
+                .map(listener -> dumpRequest(now, listener))
                 .sorted()
                 .collect(Collectors.joining("\n"));
     }
