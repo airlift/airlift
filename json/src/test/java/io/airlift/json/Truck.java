0188ccf3f6541e3f02156df59ae1ab68a843dac9
praveenkrishna <praveenkrishna@tutanota.com>
2019-07-09 18:27:30 +0530

    Use ObjectReader and ObjectWriter instead of ObjectMapper APIs

---
+++ json/src/test/java/io/airlift/json/Truck.java
@@ -0,0 +1,51 @@
+package io.airlift.json;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
+public class Truck
+        implements Vehicle
+{
+    private final String name;
+
+    @JsonCreator
+    public Truck(String name)
+    {
+        this.name = requireNonNull(name, "Name is null");
+    }
+
+    @JsonProperty
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        Truck o = (Truck) obj;
+        return Objects.equals(this.name, o.name);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString()
+    {
+        return name;
+    }
+}
