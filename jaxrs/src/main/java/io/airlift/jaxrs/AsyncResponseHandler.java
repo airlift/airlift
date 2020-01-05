69836e134dece077824e754a3acd51c069506eed
David Phillips <david@acz.org>
2020-01-04 22:31:15 -0800

    Move AsyncResponseHandler to JAX-RS module

--- jaxrs/src/main/java/io/airlift/jaxrs/AsyncResponseHandler.java
+++ jaxrs/src/main/java/io/airlift/jaxrs/AsyncResponseHandler.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.airlift.http.server;
+package io.airlift.jaxrs;
 
 import com.google.common.annotations.Beta;
 import com.google.common.util.concurrent.FutureCallback;
