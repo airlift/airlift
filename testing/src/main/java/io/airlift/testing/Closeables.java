ffd8cc42c8ecead46657665299f69d7d6c0f8b30
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2020-01-08 14:58:17 +0100

    Support AutoCloseable in Closeables

--- testing/src/main/java/io/airlift/testing/Closeables.java
+++ testing/src/main/java/io/airlift/testing/Closeables.java
@@ -1,7 +1,9 @@
 package io.airlift.testing;
 
-import java.io.Closeable;
-import java.io.IOException;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 
@@ -11,96 +13,90 @@ public final class Closeables
     {
     }
 
-    public static void closeQuietly(Closeable... closeables)
+    public static void closeQuietly(AutoCloseable... closeables)
     {
         if (closeables == null) {
             return;
         }
-        for (Closeable closeable : closeables) {
+        ImmutableList.Builder<Throwable> exceptions = ImmutableList.builder();
+        for (AutoCloseable closeable : closeables) {
             try {
                 if (closeable != null) {
                     closeable.close();
                 }
             }
-            catch (IOException | RuntimeException ignored) {
+            catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                exceptions.add(e);
+            }
+            catch (Exception ignored) {
             }
         }
+        rethrowAsRuntime(exceptions.build());
     }
 
-    public static void closeAll(Closeable... closeables)
-            throws IOException
+    public static void closeAll(AutoCloseable... closeables)
+            throws Exception
     {
         if (closeables == null) {
             return;
         }
-        IOException rootCause = null;
-        for (Closeable closeable : closeables) {
+        ImmutableList.Builder<Throwable> exceptions = ImmutableList.builder();
+        for (AutoCloseable closeable : closeables) {
             try {
                 if (closeable != null) {
                     closeable.close();
                 }
             }
-            catch (IOException e) {
-                if (rootCause == null) {
-                    rootCause = e;
-                }
-                else if (rootCause != e) {
-                    // Self-suppression not permitted
-                    rootCause.addSuppressed(e);
-                }
+            catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                exceptions.add(e);
+            }
+            catch (Exception e) {
+                exceptions.add(e);
             }
             catch (Throwable e) {
-                if (rootCause == null) {
-                    rootCause = new IOException(e);
-                }
-                else if (rootCause != e) {
-                    // Self-suppression not permitted
-                    rootCause.addSuppressed(e);
-                }
+                exceptions.add(e);
             }
         }
-        if (rootCause != null) {
-            throw rootCause;
-        }
+        rethrow(exceptions.build());
     }
 
-    public static void closeAllRuntimeException(Closeable... closeables)
+    public static void closeAllRuntimeException(AutoCloseable... closeables)
     {
         if (closeables == null) {
             return;
         }
-        RuntimeException rootCause = null;
-        for (Closeable closeable : closeables) {
+        ImmutableList.Builder<Throwable> exceptions = ImmutableList.builder();
+        for (AutoCloseable closeable : closeables) {
             try {
                 if (closeable != null) {
                     closeable.close();
                 }
             }
             catch (Throwable e) {
-                if (rootCause == null) {
-                    rootCause = new RuntimeException(e);
-                }
-                else if (rootCause != e) {
-                    // Self-suppression not permitted
-                    rootCause.addSuppressed(e);
-                }
+                exceptions.add(e);
             }
         }
-        if (rootCause != null) {
-            throw rootCause;
-        }
+        rethrowAsRuntime(exceptions.build());
     }
 
-    public static <T extends Throwable> T closeAllSuppress(T rootCause, Closeable... closeables)
+    public static <T extends Throwable> T closeAllSuppress(T rootCause, AutoCloseable... closeables)
     {
         requireNonNull(rootCause, "rootCause is null");
         if (closeables == null) {
             return rootCause;
         }
-        for (Closeable closeable : closeables) {
+        for (AutoCloseable closeable : closeables) {
             try {
                 if (closeable != null) {
                     closeable.close();
+                }
+            }
+            catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                if (rootCause != e) {
+                    rootCause.addSuppressed(e);
                 }
             }
             catch (Throwable e) {
@@ -111,5 +107,57 @@ public final class Closeables
             }
         }
         return rootCause;
+    }
+
+    private static void rethrowAsRuntime(List<Throwable> exceptions)
+    {
+        Optional<RuntimeException> rootException = exceptions.stream()
+                .filter(RuntimeException.class::isInstance)
+                .map(RuntimeException.class::cast)
+                .findFirst();
+
+        if (!rootException.isPresent() && !exceptions.isEmpty()) {
+            rootException = Optional.of(new RuntimeException());
+        }
+
+        if (rootException.isPresent()) {
+            addSuppressed(rootException.get(), exceptions);
+            throw rootException.get();
+        }
+    }
+
+    private static <E extends Exception> void rethrow(List<Throwable> exceptions)
+            throws Exception
+    {
+        Optional<Exception> rootException = exceptions.stream()
+                .filter(InterruptedException.class::isInstance)
+                .map(Exception.class::cast)
+                .findFirst();
+
+        if (!rootException.isPresent()) {
+            rootException = exceptions.stream()
+                    .filter(Exception.class::isInstance)
+                    .map(Exception.class::cast)
+                    .findFirst();
+        }
+
+        if (!rootException.isPresent() && !exceptions.isEmpty()) {
+            rootException = Optional.of(new Exception());
+        }
+
+        if (rootException.isPresent()) {
+            addSuppressed(rootException.get(), exceptions);
+            throw rootException.get();
+        }
+    }
+
+    private static void addSuppressed(Exception rootException, List<Throwable> exceptions)
+    {
+        for (Throwable exception : exceptions) {
+            if (rootException != exception) {
+                // Self-suppression not permitted
+                rootException.addSuppressed(exception);
+            }
+        }
     }
 }
