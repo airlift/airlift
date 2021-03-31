427501501d566e468f9410eb973965cc57ac6df3
Dain Sundstrom <dain@iq80.com>
2021-03-30 21:03:49 -0700

    Add builder for BodyGenerator containing form data

---
+++ http-client/src/main/java/io/airlift/http/client/FormDataBodyBuilder.java
@@ -0,0 +1,26 @@
+package io.airlift.http.client;
+
+import static com.google.common.net.UrlEscapers.urlFormParameterEscaper;
+import static io.airlift.http.client.StaticBodyGenerator.createStaticBodyGenerator;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class FormDataBodyBuilder
+{
+    private final StringBuilder buffer = new StringBuilder();
+
+    public FormDataBodyBuilder addField(String name, String value)
+    {
+        if (buffer.length() > 0) {
+            buffer.append("&");
+        }
+        buffer.append(urlFormParameterEscaper().escape(name))
+                .append("=")
+                .append(urlFormParameterEscaper().escape(value));
+        return this;
+    }
+
+    public StaticBodyGenerator build()
+    {
+        return createStaticBodyGenerator(buffer.toString(), UTF_8);
+    }
+}
