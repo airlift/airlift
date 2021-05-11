95bf274f22a36bbf499f342f1780107f0aaa0d68
Łukasz Walkiewicz <lukasz.walkiewicz@starburstdata.com>
2021-05-11 23:11:15 +0200

    Fix formatting

--- http-server/src/main/java/io/airlift/http/server/HttpServer.java
+++ http-server/src/main/java/io/airlift/http/server/HttpServer.java
@@ -88,7 +88,8 @@ import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
 
 public class HttpServer
 {
-    public enum ClientCertificate {
+    public enum ClientCertificate
+    {
         NONE, REQUESTED, REQUIRED
     }
 
