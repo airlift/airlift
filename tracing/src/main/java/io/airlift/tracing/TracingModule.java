518744374364a6139cee12cf1dd8a579875f84c6
David Phillips <david@acz.org>
2024-03-12 15:28:52 -0700

    Move Tracer provider to OpenTelemetryModule

--- tracing/src/main/java/io/airlift/tracing/TracingModule.java
+++ tracing/src/main/java/io/airlift/tracing/TracingModule.java
@@ -1,13 +1,10 @@
 package io.airlift.tracing;
 
 import com.google.inject.Binder;
-import com.google.inject.Provides;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
 import io.airlift.tracing.SpanSerialization.SpanDeserializer;
 import io.airlift.tracing.SpanSerialization.SpanSerializer;
-import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.Tracer;
 
 import static io.airlift.json.JsonBinder.jsonBinder;
 import static java.util.Objects.requireNonNull;
@@ -35,11 +32,5 @@ public class TracingModule
 
         jsonBinder(binder).addSerializerBinding(Span.class).to(SpanSerializer.class);
         jsonBinder(binder).addDeserializerBinding(Span.class).to(SpanDeserializer.class);
-    }
-
-    @Provides
-    public Tracer createTracer(OpenTelemetry openTelemetry)
-    {
-        return openTelemetry.getTracer(serviceName);
     }
 }
