aaad86ea1e98da372c93fc874bcb45bb0e84c2f6
Łukasz Osipiuk <lukasz@osipiuk.net>
2024-03-18 13:20:58 +0100

    Fix setting limits JSON reader limits
    If limits are set via setStreamReadConstraints on JsonFactory it is not
    effective for maxStringLength and maxNameLenght.
    Those are checked via _rootCharSymbols field of JsonFactory which
    has a copy of StreamReadConstraints, as of when JsonFactory was created.
    It is not reset when we call setStreamReadConstraints on JsonFactory.

---
+++ json/src/test/java/io/airlift/json/TestLimits.java
@@ -0,0 +1,67 @@
+package io.airlift.json;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestLimits
+{
+    @Test
+    public void testNameLimitDefaultJsonFactory()
+            throws IOException
+    {
+        testNameLengthLimit(new ObjectMapperProvider());
+    }
+
+    @Test
+    public void testNameLimitCustomJsonFactory()
+            throws IOException
+    {
+        JsonFactory myJsonFactory = new JsonFactory();
+        testNameLengthLimit(new ObjectMapperProvider(myJsonFactory));
+    }
+
+    private void testNameLengthLimit(ObjectMapperProvider objectMapperProvider)
+            throws IOException
+    {
+        ObjectMapper objectMapper = objectMapperProvider.get();
+        String longName = Strings.repeat("a", 100000);
+
+        String content = String.format("{ \"%s\" : \"value\" }", longName);
+        JsonNode jsonNode = objectMapper.reader().readTree(content);
+        assertThat(jsonNode.has(longName)).isTrue();
+        assertThat(jsonNode.findValue(longName).asText()).isEqualTo("value");
+    }
+
+    @Test
+    public void testStringLimitDefaultJsonFactory()
+            throws IOException
+    {
+        testNameLengthLimit(new ObjectMapperProvider());
+    }
+
+    @Test
+    public void testStringLimitCustomJsonFactory()
+            throws IOException
+    {
+        JsonFactory myJsonFactory = new JsonFactory();
+        testNameLengthLimit(new ObjectMapperProvider(myJsonFactory));
+    }
+
+    private void testStringLengthLimit(ObjectMapperProvider objectMapperProvider)
+            throws IOException
+    {
+        ObjectMapper objectMapper = objectMapperProvider.get();
+        String longValue = Strings.repeat("a", 100000);
+
+        String content = String.format("{ \"key\" : \"%s\" }", longValue);
+        JsonNode jsonNode = objectMapper.reader().readTree(content);
+        assertThat(jsonNode.findValue("key").asText()).isEqualTo(longValue);
+    }
+}
