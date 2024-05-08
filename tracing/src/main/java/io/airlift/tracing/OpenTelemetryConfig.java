b1d838e0ec50cd6f42e051f75bff3d098cdde946
vinay-kosaraju <vinay.kosaraju@starburstdata.com>
2024-05-07 21:34:22 -0500

    Add support for probabilistic sampling in Tracing

---
+++ tracing/src/main/java/io/airlift/tracing/OpenTelemetryConfig.java
@@ -0,0 +1,24 @@
+package io.airlift.tracing;
+
+import io.airlift.configuration.Config;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
+public class OpenTelemetryConfig
+{
+    private double samplingRatio = 1;
+
+    @Max(1)
+    @Min(0)
+    public double getSamplingRatio()
+    {
+        return samplingRatio;
+    }
+
+    @Config("tracing.sampling-ratio")
+    public OpenTelemetryConfig setSamplingRatio(Double ratio)
+    {
+        this.samplingRatio = ratio;
+        return this;
+    }
+}
