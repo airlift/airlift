0d8910ae27d5a9773c75a1999d2f28cb94839999
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2024-05-25 18:14:02 +0200

    Simplify TestServiceInventory

--- discovery/src/test/java/io/airlift/discovery/client/TestServiceInventory.java
+++ discovery/src/test/java/io/airlift/discovery/client/TestServiceInventory.java
@@ -35,9 +35,6 @@ import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.URI;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.testng.Assert.assertEquals;
@@ -91,12 +88,6 @@ public class TestServiceInventory
         Server server = null;
         // HTTP/2 disabled since there is no H2C in the test server
         try (JettyHttpClient httpClient = new JettyHttpClient(new HttpClientConfig().setHttp2Enabled(false))) {
-            int port;
-            try (ServerSocket socket = new ServerSocket()) {
-                socket.bind(new InetSocketAddress(0));
-                port = socket.getLocalPort();
-            }
-            URI baseURI = new URI("http", null, "127.0.0.1", port, null, null, null);
             HttpConfiguration httpConfiguration = new HttpConfiguration();
             httpConfiguration.setSendServerVersion(false);
             httpConfiguration.setSendXPoweredBy(false);
@@ -104,7 +95,7 @@ public class TestServiceInventory
             server = new Server();
 
             ServerConnector httpConnector = new ServerConnector(server, new HttpConnectionFactory(httpConfiguration));
-            httpConnector.setPort(port);
+            httpConnector.setPort(0);
             httpConnector.setName("http");
             server.addConnector(httpConnector);
 
@@ -119,7 +110,7 @@ public class TestServiceInventory
 
             // test
             ServiceInventoryConfig serviceInventoryConfig = new ServiceInventoryConfig()
-                    .setServiceInventoryUri(baseURI);
+                    .setServiceInventoryUri(server.getURI());
 
             ServiceInventory serviceInventory = new ServiceInventory(serviceInventoryConfig,
                     new NodeInfo("test"),
