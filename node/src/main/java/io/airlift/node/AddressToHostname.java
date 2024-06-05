a97371ae60d6a9a5db85f528de22d6d66a1eabbd
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2024-06-05 21:45:55 +0200

    Update airbase to 157

--- node/src/main/java/io/airlift/node/AddressToHostname.java
+++ node/src/main/java/io/airlift/node/AddressToHostname.java
@@ -16,6 +16,7 @@ package io.airlift.node;
 import com.google.common.net.InetAddresses;
 
 import java.net.Inet4Address;
+import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Optional;
@@ -28,16 +29,30 @@ public final class AddressToHostname
 
     public static String encodeAddressAsHostname(InetAddress inetAddress)
     {
-        String ipString = InetAddresses.toAddrString(inetAddress);
-        if (inetAddress instanceof Inet4Address) {
-            ipString = ipString.replace('.', '-');
-        }
-        else {
+        String hostname = switch (inetAddress) {
+            case Inet4Address inet4Address -> InetAddresses
+                    .toAddrString(inet4Address)
+                    .replace('.', '-');
+
             // v6 addresses contain `:` which isn't legal, also v6 can
             // start with a `:` so we prefix with an `x`
-            ipString = "x" + ipString.replace(':', '-');
+            case Inet6Address inet6Address -> 'x' + stripScopeId(InetAddresses.toAddrString(inet6Address))
+                    .replace(':', '-');
+
+            default -> InetAddresses.toAddrString(inetAddress);
+        };
+
+        return hostname + IP_ENCODED_SUFFIX;
+    }
+
+    // Strips scope ID added to toAddrString representation in https://github.com/google/guava/commit/3f61870ac6e5b18dbb74ce6f6cb2930ad8750a43
+    private static String stripScopeId(String address)
+    {
+        int index = address.indexOf('%');
+        if (index == -1) {
+            return address;
         }
-        return ipString + IP_ENCODED_SUFFIX;
+        return address.substring(0, index);
     }
 
     public static Optional<InetAddress> tryDecodeHostnameToAddress(String hostname)
