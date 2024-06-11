06b7b814684f4dce038c5a7424f61c95cfa07770
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2024-06-11 18:36:29 +0200

    Test that client can explicitly downgrade protocol

--- http-client/src/test/java/io/airlift/http/client/jetty/TestJettyHttpClientHttp2.java
+++ http-client/src/test/java/io/airlift/http/client/jetty/TestJettyHttpClientHttp2.java
@@ -1,7 +1,6 @@
 package io.airlift.http.client.jetty;
 
 import io.airlift.http.client.HttpClientConfig;
-import io.airlift.http.client.HttpVersion;
 import io.airlift.http.client.Request;
 import io.airlift.http.client.ResponseHandler;
 
@@ -19,13 +18,13 @@ public class TestJettyHttpClientHttp2
     public <T, E extends Exception> T executeRequest(HttpClientConfig config, Request request, ResponseHandler<T, E> responseHandler)
             throws Exception
     {
-        return super.executeRequest(config, upgradeRequest(request, HttpVersion.HTTP_2), responseHandler);
+        return super.executeRequest(config, request, responseHandler);
     }
 
     @Override
     public <T, E extends Exception> T executeRequest(Request request, ResponseHandler<T, E> responseHandler)
             throws Exception
     {
-        return super.executeRequest(upgradeRequest(request, HttpVersion.HTTP_2), responseHandler);
+        return super.executeRequest(request, responseHandler);
     }
 }
