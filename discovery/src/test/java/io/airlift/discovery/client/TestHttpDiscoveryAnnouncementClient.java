4669f55660267034374c6c9f34377ac0a99e58d4
Walter Weiss <walter.weiss@starburstdata.com>
2025-06-12 09:56:43 +0200

    Detect a change in the location of the Discovery Service
    The Discovery Service domain name is resolved every time a new
    announcement is generated. While it is intentional that a Discovery
    Service can be moved to a new host, it is desirable to detect when this
    happens. The new log generated with a change of address serves both as
    an observational tool and a security detection tool.

--- discovery/src/test/java/io/airlift/discovery/client/TestHttpDiscoveryAnnouncementClient.java
+++ discovery/src/test/java/io/airlift/discovery/client/TestHttpDiscoveryAnnouncementClient.java
@@ -2,9 +2,12 @@ package io.airlift.discovery.client;
 
 import org.junit.jupiter.api.Test;
 
+import java.net.InetAddress;
 import java.net.URI;
+import java.util.Optional;
 
 import static io.airlift.discovery.client.HttpDiscoveryAnnouncementClient.createAnnouncementLocation;
+import static io.airlift.discovery.client.HttpDiscoveryAnnouncementClient.reportChangedAddressResolution;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestHttpDiscoveryAnnouncementClient
@@ -19,5 +22,53 @@ public class TestHttpDiscoveryAnnouncementClient
         expected = URI.create("https://example.com:8080/v1/announcement/abc");
         assertThat(createAnnouncementLocation(URI.create("https://example.com:8080"), "abc")).isEqualTo(expected);
         assertThat(createAnnouncementLocation(URI.create("https://example.com:8080/"), "abc")).isEqualTo(expected);
+    }
+
+    @Test
+    public void testResolutionFailed()
+    {
+        URI badUri = URI.create("https://example.bad:8080/");
+        Optional<InetAddress> result = reportChangedAddressResolution(badUri, Optional.empty(), false);
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void testPortChangeIgnored()
+    {
+        URI uri1 = URI.create("https://example.com:8080/");
+        URI uri2 = URI.create("https://example.com:80/");
+        Optional<InetAddress> success = reportChangedAddressResolution(uri1, Optional.empty(), true);
+        assertThat(success).isNotEmpty();
+        assertThat(reportChangedAddressResolution(uri2, success, true)).isEqualTo(success);
+    }
+
+    @Test
+    public void testSchemeChangeIgnored()
+    {
+        URI uri1 = URI.create("http://example.com:8080/");
+        URI uri2 = URI.create("https://example.com:8080/");
+        Optional<InetAddress> success = reportChangedAddressResolution(uri1, Optional.empty(), true);
+        assertThat(success).isNotEmpty();
+        assertThat(reportChangedAddressResolution(uri2, success, true)).isEqualTo(success);
+    }
+
+    @Test
+    public void testPathChangeIgnored()
+    {
+        URI uri1 = URI.create("http://example.com:8080/api/v1/announcement/abc");
+        URI uri2 = URI.create("https://example.com:8080/");
+        Optional<InetAddress> success = reportChangedAddressResolution(uri1, Optional.empty(), true);
+        assertThat(success).isNotEmpty();
+        assertThat(reportChangedAddressResolution(uri2, success, true)).isEqualTo(success);
+    }
+
+    @Test
+    public void testAddressChangeDetected()
+    {
+        URI uri1 = URI.create("https://example.com:8080/");
+        URI uri2 = URI.create("https://google.com:80/");
+        Optional<InetAddress> firstResolution = reportChangedAddressResolution(uri1, Optional.empty(), true);
+        assertThat(firstResolution).isNotEmpty();
+        assertThat(reportChangedAddressResolution(uri2, firstResolution, true)).isNotEqualTo(firstResolution);
     }
 }
