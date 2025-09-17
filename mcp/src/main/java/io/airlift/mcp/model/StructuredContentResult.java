6ab822981c30f47b4a1ae33f0e3701f862631916
Cody Zwiefelhofer <cody.zwiefelhofer@starburstdata.com>
2025-09-17 07:54:18 -0500

    Make MCP structured content optional
    There may be cases in errors during tool execution where the structured output may not make sense, e.g. parameter validation.
    This commit makes structured output optional, as most clients would ignore it any way in favor of the `content` when `isError` = true

--- mcp/src/main/java/io/airlift/mcp/model/StructuredContentResult.java
+++ mcp/src/main/java/io/airlift/mcp/model/StructuredContentResult.java
@@ -3,14 +3,20 @@ package io.airlift.mcp.model;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 
-public record StructuredContentResult<T>(List<Content> content, T structuredContent, boolean isError)
+public record StructuredContentResult<T>(List<Content> content, Optional<T> structuredContent, boolean isError)
 {
     public StructuredContentResult
     {
         content = ImmutableList.copyOf(content);
         requireNonNull(structuredContent, "structuredContent is null");
+    }
+
+    public StructuredContentResult(List<Content> content, T structuredContent, boolean isError)
+    {
+        this(content, Optional.ofNullable(structuredContent), isError);
     }
 }
