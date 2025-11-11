005fad25b692785a6c7ac4201f117b9e0e7231b3
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-11-11 07:51:28 +0100

    Avoid using raw type

--- configuration/src/main/java/io/airlift/configuration/ConfigurationFactory.java
+++ configuration/src/main/java/io/airlift/configuration/ConfigurationFactory.java
@@ -182,7 +182,7 @@ public class ConfigurationFactory
                 @Override
                 public <T> Void visit(Binding<T> binding)
                 {
-                    if (binding instanceof InstanceBinding instanceBinding) {
+                    if (binding instanceof InstanceBinding<?> instanceBinding) {
                         // configuration listener
                         if (instanceBinding.getInstance() instanceof ConfigurationBindingListenerHolder) {
                             addConfigurationBindingListener(((ConfigurationBindingListenerHolder) instanceBinding.getInstance()).getConfigurationBindingListener());
@@ -197,7 +197,7 @@ public class ConfigurationFactory
                     // configuration provider
                     if (binding instanceof ProviderInstanceBinding<?> providerInstanceBinding) {
                         Provider<?> provider = providerInstanceBinding.getProviderInstance();
-                        if (provider instanceof ConfigurationProvider configurationProvider) {
+                        if (provider instanceof ConfigurationProvider<?> configurationProvider) {
                             registerConfigurationProvider(configurationProvider, Optional.of(binding.getSource()));
                         }
                     }
