a813575927fbcce3f8fa7f3b9d268221c3cca382
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-11-11 08:01:20 +0100

    Inline local variables

--- configuration/src/test/java/io/airlift/configuration/TestConfig.java
+++ configuration/src/test/java/io/airlift/configuration/TestConfig.java
@@ -327,7 +327,7 @@ public class TestConfig
     @SafeVarargs
     private static <T> Module createModule(Key<T> key, Class<T> configClass, String prefix, ConfigDefaults<T>... configDefaults)
     {
-        Module module = binder -> {
+        return binder -> {
             ConfigBinder configBinder = configBinder(binder);
 
             configBinder.bindConfig(key, configClass, prefix);
@@ -336,8 +336,6 @@ public class TestConfig
                 configBinder.bindConfigDefaults(key, configDefault);
             }
         };
-
-        return module;
     }
 
     private static Map<String, String> prefix(String prefix, Map<String, String> properties)
