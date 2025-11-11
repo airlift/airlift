96a7712e01679f3bbb212d04fa3c40e4c8cd8ddc
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-11-11 07:13:56 +0100

    Use enhanced switch expression

--- configuration/src/test/java/io/airlift/configuration/TestConfig.java
+++ configuration/src/test/java/io/airlift/configuration/TestConfig.java
@@ -298,15 +298,10 @@ public class TestConfig
                 install(SwitchModule.switchModule(
                         SwitchConfig.class,
                         SwitchConfig::getValue,
-                        value -> {
-                            switch (value) {
-                                case A:
-                                    return a;
-                                case B:
-                                    return b;
-                                default:
-                                    throw new RuntimeException("Not supported value: " + value);
-                            }
+                        value -> switch (value) {
+                            case A -> a;
+                            case B -> b;
+                            default -> throw new RuntimeException("Not supported value: " + value);
                         }));
             }
         };
