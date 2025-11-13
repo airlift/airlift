3d99611fbda1ebc28a94393d8cba64a39c8173b5
Jordan Zimmerman <jordan@jordanzimmerman.com>
2025-11-13 08:28:54 +0000

    Support server-to-client notifications during a request
    The reference MCP server does not support server-to-client messages.
    This change wraps the servlet objects sent to the reference implementation's
    Servlet so that we can change the response to an SSE response for the
    purpose of supporting server-to-client messages. The response is
    changed only if a message is actually sent.
    
    Added tests for this.
    
    Note: this takes advantage of inner knowledge of how `HttpServletStatelessServerTransport`
    works. Hopefully the MCP team will add native support for this in the
    future. We've opened PRs with possible implementations and have started
    GitHub discussions about future features.

--- mcp/src/main/java/io/airlift/mcp/handler/PromptHandler.java
+++ mcp/src/main/java/io/airlift/mcp/handler/PromptHandler.java
@@ -1,10 +1,10 @@
 package io.airlift.mcp.handler;
 
+import io.airlift.mcp.McpRequestContext;
 import io.airlift.mcp.model.GetPromptRequest;
 import io.airlift.mcp.model.GetPromptResult;
-import jakarta.servlet.http.HttpServletRequest;
 
 public interface PromptHandler
 {
-    GetPromptResult getPrompt(HttpServletRequest request, GetPromptRequest getPromptRequest);
+    GetPromptResult getPrompt(McpRequestContext requestContext, GetPromptRequest getPromptRequest);
 }
