f2122a17b93fb4028bb67f1a124a46aedb38d52c
Jordan Zimmerman <jordan@jordanzimmerman.com>
2025-11-15 09:23:00 +0000

    Support MCP completions
    - Added two new annotations and general handler support for MCP completions.
    - Added a few examples in the test endpoints
    - Added tests and doc

--- mcp/src/main/java/io/airlift/mcp/McpMetadata.java
+++ mcp/src/main/java/io/airlift/mcp/McpMetadata.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
 
-public record McpMetadata(String uriPath, Implementation implementation, Optional<String> instructions, boolean tools, boolean prompts, boolean resources)
+public record McpMetadata(String uriPath, Implementation implementation, Optional<String> instructions, boolean tools, boolean prompts, boolean resources, boolean completions)
 {
     public static final String CONTEXT_REQUEST_KEY = McpMetadata.class.getName() + ".request";
 
@@ -19,31 +19,36 @@ public record McpMetadata(String uriPath, Implementation implementation, Optiona
 
     public McpMetadata(String uriPath)
     {
-        this(uriPath, new Implementation("mcp", "1.0.0"), Optional.empty(), false, false, false);
+        this(uriPath, new Implementation("mcp", "1.0.0"), Optional.empty(), false, false, false, false);
     }
 
     public McpMetadata withImplementation(Implementation implementation)
     {
-        return new McpMetadata(uriPath, implementation, instructions, tools, prompts, resources);
+        return new McpMetadata(uriPath, implementation, instructions, tools, prompts, resources, completions);
     }
 
     public McpMetadata withInstructions(String instructions)
     {
-        return new McpMetadata(uriPath, implementation, Optional.ofNullable(instructions), tools, prompts, resources);
+        return new McpMetadata(uriPath, implementation, Optional.ofNullable(instructions), tools, prompts, resources, completions);
     }
 
     public McpMetadata withTools(boolean tools)
     {
-        return new McpMetadata(uriPath, implementation, instructions, tools, prompts, resources);
+        return new McpMetadata(uriPath, implementation, instructions, tools, prompts, resources, completions);
     }
 
     public McpMetadata withPrompts(boolean prompts)
     {
-        return new McpMetadata(uriPath, implementation, instructions, tools, prompts, resources);
+        return new McpMetadata(uriPath, implementation, instructions, tools, prompts, resources, completions);
     }
 
     public McpMetadata withResources(boolean resources)
     {
-        return new McpMetadata(uriPath, implementation, instructions, tools, prompts, resources);
+        return new McpMetadata(uriPath, implementation, instructions, tools, prompts, resources, completions);
+    }
+
+    public McpMetadata withCompletions(boolean completions)
+    {
+        return new McpMetadata(uriPath, implementation, instructions, tools, prompts, resources, completions);
     }
 }
