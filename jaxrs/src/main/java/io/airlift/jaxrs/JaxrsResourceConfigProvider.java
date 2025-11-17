5787108196c03b816c24909fc3fd104e96de64ff
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-11-17 21:23:58 +0100

    Disable outbound buffering

--- jaxrs/src/main/java/io/airlift/jaxrs/JaxrsResourceConfigProvider.java
+++ jaxrs/src/main/java/io/airlift/jaxrs/JaxrsResourceConfigProvider.java
@@ -13,6 +13,7 @@ import java.util.Set;
 
 import static io.airlift.jaxrs.BinderUtils.qualifiedKey;
 import static java.util.Objects.requireNonNull;
+import static org.glassfish.jersey.CommonProperties.OUTBOUND_CONTENT_LENGTH_BUFFER;
 import static org.glassfish.jersey.server.ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR;
 
 public class JaxrsResourceConfigProvider
@@ -37,6 +38,8 @@ public class JaxrsResourceConfigProvider
     {
         Set<Object> singletons = injector.getInstance(qualifiedKey(qualifier, new TypeLiteral<>() {}));
         return new JaxrsResourceConfig(singletons)
-                .setProperties(ImmutableMap.of(RESPONSE_SET_STATUS_OVER_SEND_ERROR, "true"));
+                .setProperties(ImmutableMap.of(
+                        RESPONSE_SET_STATUS_OVER_SEND_ERROR, "true",
+                        OUTBOUND_CONTENT_LENGTH_BUFFER, "-1"));
     }
 }
