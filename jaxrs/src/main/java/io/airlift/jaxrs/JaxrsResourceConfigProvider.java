9b4c10893955838369d2d23a5a699626c77c95ca
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-11-17 21:23:58 +0100

    Disable outbound buffering
    Jetty http server buffers output when writing which makes Jersey server-side buffering redundant

--- jaxrs/src/main/java/io/airlift/jaxrs/JaxrsResourceConfigProvider.java
+++ jaxrs/src/main/java/io/airlift/jaxrs/JaxrsResourceConfigProvider.java
@@ -13,6 +13,7 @@ import java.util.Set;
 
 import static io.airlift.jaxrs.BinderUtils.qualifiedKey;
 import static java.util.Objects.requireNonNull;
+import static org.glassfish.jersey.CommonProperties.OUTBOUND_CONTENT_LENGTH_BUFFER;
 import static org.glassfish.jersey.server.ServerProperties.RESPONSE_SET_STATUS_OVER_SEND_ERROR;
 
 public class JaxrsResourceConfigProvider
@@ -37,6 +38,12 @@ public class JaxrsResourceConfigProvider
     {
         Set<Object> singletons = injector.getInstance(qualifiedKey(qualifier, new TypeLiteral<>() {}));
         return new JaxrsResourceConfig(singletons)
-                .setProperties(ImmutableMap.of(RESPONSE_SET_STATUS_OVER_SEND_ERROR, "true"));
+                .setProperties(ImmutableMap.of(
+                        RESPONSE_SET_STATUS_OVER_SEND_ERROR, "true",
+                        // Jetty http server buffers output when writing which makes Jersey server-side buffering redundant.
+                        // For small responses, allocating 8KB buffer is wasteful. For large responses, Jetty will buffer
+                        // as needed. Having Content-Length for small responses is not critical as in the HTTP/2
+                        // Content-Length can be inferred from DATA frame length.
+                        OUTBOUND_CONTENT_LENGTH_BUFFER, "0"));
     }
 }
