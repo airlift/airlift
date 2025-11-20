cbddab784b231e242508f612f1ac7aaf9df24b70
Jordan Zimmerman <jordan@jordanzimmerman.com>
2025-11-20 11:42:20 +0000

    HttpServletStatelessServerTransport for ReferenceServerTransport
    It's getting too hard to manipulate `HttpServletStatelessServerTransport`.
    It doesn't do that much anyway. So, let's use our own implementation
    based off of it.

--- mcp/src/main/java/io/airlift/mcp/reference/ReferenceModule.java
+++ mcp/src/main/java/io/airlift/mcp/reference/ReferenceModule.java
@@ -1,20 +1,17 @@
 package io.airlift.mcp.reference;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.ImmutableMap;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import io.airlift.mcp.McpMetadata;
 import io.airlift.mcp.handler.RequestContextProvider;
-import io.modelcontextprotocol.common.McpTransportContext;
 import io.modelcontextprotocol.json.McpJsonMapper;
 import io.modelcontextprotocol.json.jackson.JacksonMcpJsonMapper;
 import io.modelcontextprotocol.server.McpServer;
 import io.modelcontextprotocol.server.McpServer.StatelessSyncSpecification;
 import io.modelcontextprotocol.server.McpStatelessSyncServer;
-import io.modelcontextprotocol.server.transport.HttpServletStatelessServerTransport;
 import io.modelcontextprotocol.spec.McpSchema;
 import io.modelcontextprotocol.util.DefaultMcpUriTemplateManagerFactory;
 import io.modelcontextprotocol.util.McpUriTemplateManagerFactory;
@@ -22,7 +19,6 @@ import jakarta.servlet.Filter;
 
 import static com.google.inject.Scopes.SINGLETON;
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
-import static io.airlift.mcp.McpMetadata.CONTEXT_REQUEST_KEY;
 
 public class ReferenceModule
         implements Module
@@ -31,6 +27,7 @@ public class ReferenceModule
     public void configure(Binder binder)
     {
         binder.bind(ReferenceServer.class).asEagerSingleton();
+        binder.bind(ReferenceServerTransport.class).asEagerSingleton();
         binder.bind(io.airlift.mcp.McpServer.class).to(ReferenceServer.class).in(SINGLETON);
         newSetBinder(binder, Filter.class).addBinding().to(ReferenceFilter.class).in(SINGLETON);
         binder.bind(McpUriTemplateManagerFactory.class).to(DefaultMcpUriTemplateManagerFactory.class).in(SINGLETON);
@@ -39,18 +36,7 @@ public class ReferenceModule
 
     @Singleton
     @Provides
-    public HttpServletStatelessServerTransport mcpTransport(McpMetadata metadata, McpJsonMapper objectMapper)
-    {
-        return HttpServletStatelessServerTransport.builder()
-                .messageEndpoint(metadata.uriPath())
-                .jsonMapper(objectMapper)
-                .contextExtractor(request -> McpTransportContext.create(ImmutableMap.of(CONTEXT_REQUEST_KEY, request)))
-                .build();
-    }
-
-    @Singleton
-    @Provides
-    public McpStatelessSyncServer buildServer(HttpServletStatelessServerTransport transport, McpMetadata metadata, McpJsonMapper objectMapper, McpUriTemplateManagerFactory uriTemplateManagerFactory)
+    public McpStatelessSyncServer buildServer(ReferenceServerTransport transport, McpMetadata metadata, McpJsonMapper mcpJsonMapper, McpUriTemplateManagerFactory uriTemplateManagerFactory)
     {
         McpSchema.ServerCapabilities serverCapabilities = new McpSchema.ServerCapabilities(
                 null,
@@ -61,7 +47,7 @@ public class ReferenceModule
                 metadata.tools() ? new McpSchema.ServerCapabilities.ToolCapabilities(false) : null);
 
         StatelessSyncSpecification builder = McpServer.sync(transport)
-                .jsonMapper(objectMapper)
+                .jsonMapper(mcpJsonMapper)
                 .capabilities(serverCapabilities)
                 .uriTemplateManagerFactory(uriTemplateManagerFactory)
                 .serverInfo(metadata.implementation().name(), metadata.implementation().version());
