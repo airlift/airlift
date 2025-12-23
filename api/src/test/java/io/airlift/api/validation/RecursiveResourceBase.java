ff06e0c93c9978583e2affc68d8067c0dda4b752
Jordan Zimmerman <jordan@jordanzimmerman.com>
2025-12-23 11:20:47 +0000

    Self-recursive references could cause NPE
    `typeToResourceCache` won't contain references to the currently
    active resource. Thus, if that resource is recursive on itself it
    would generate an NPE. Fix this by adding to the cache early.
    
    Fixes #1689

--- api/src/test/java/io/airlift/api/validation/RecursiveResourceBase.java
+++ api/src/test/java/io/airlift/api/validation/RecursiveResourceBase.java
@@ -2,6 +2,7 @@ package io.airlift.api.validation;
 
 import io.airlift.api.ApiDescription;
 import io.airlift.api.ApiPolyResource;
+import io.airlift.api.ApiReadOnly;
 import io.airlift.api.ApiResource;
 
 import java.util.List;
@@ -12,6 +13,12 @@ public sealed interface RecursiveResourceBase
 {
     @ApiResource(name = "actualResource", description = "foo")
     record ActualResource(
-            @ApiDescription("foo") Optional<List<RecursiveResourceBase>> nested)
-            implements RecursiveResourceBase {}
+            @ApiDescription("foo") Optional<List<RecursiveResourceBase>> nested,
+            @ApiDescription("foo") List<ActualResource> selfList,
+            @ApiDescription("foo") List<InnerRecursive> innerRecursive)
+            implements RecursiveResourceBase
+    {
+        @ApiResource(name = "innerRecursive", description = "A resource containing a list of itself")
+        public record InnerRecursive(@ApiReadOnly @ApiDescription("Inner name") List<InnerRecursive> nested) {}
+    }
 }
