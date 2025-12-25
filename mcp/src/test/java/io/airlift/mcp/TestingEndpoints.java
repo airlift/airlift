85b08cc6ad0b5620e75e0774e31aaa220033c615
Jordan Zimmerman <jordan@jordanzimmerman.com>
2025-12-25 20:09:15 +0000

    Add list/resource change notifications
    Integrate session values into real sessions. Send tool/prompt/resource
    list changes. Support resource subscription requests, etc.

--- mcp/src/test/java/io/airlift/mcp/TestingEndpoints.java
+++ mcp/src/test/java/io/airlift/mcp/TestingEndpoints.java
@@ -1,19 +1,28 @@
 package io.airlift.mcp;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.inject.Inject;
+import io.airlift.mcp.handler.PromptEntry;
+import io.airlift.mcp.handler.ResourceEntry;
+import io.airlift.mcp.handler.ToolEntry;
 import io.airlift.mcp.model.CallToolResult;
 import io.airlift.mcp.model.CompleteRequest.CompleteArgument;
 import io.airlift.mcp.model.CompleteRequest.CompleteContext;
 import io.airlift.mcp.model.Content;
 import io.airlift.mcp.model.LoggingLevel;
+import io.airlift.mcp.model.Prompt;
 import io.airlift.mcp.model.ReadResourceRequest;
+import io.airlift.mcp.model.Resource;
 import io.airlift.mcp.model.ResourceContents;
 import io.airlift.mcp.model.ResourceTemplateValues;
 import io.airlift.mcp.model.StructuredContentResult;
+import io.airlift.mcp.model.Tool;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
@@ -25,11 +34,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TestingEndpoints
 {
     private final McpServer mcpServer;
+    private final Set<ToolEntry> tools;
+    private final Set<PromptEntry> prompts;
+    private final Set<ResourceEntry> resources;
+    private volatile String example2Content = "This is the content of file://example2.txt";
+    private volatile String example1Content = "This is the content of file://example1.txt";
 
     @Inject
-    public TestingEndpoints(McpServer mcpServer)
+    public TestingEndpoints(
+            McpServer mcpServer,
+            Set<ToolEntry> tools,
+            Set<PromptEntry> prompts,
+            Set<ResourceEntry> resources)
     {
         this.mcpServer = requireNonNull(mcpServer, "mcpServer is null");
+
+        this.tools = ImmutableSet.copyOf(tools);
+        this.prompts = ImmutableSet.copyOf(prompts);
+        this.resources = ImmutableSet.copyOf(resources);
     }
 
     @McpTool(name = "add", description = "Add two numbers")
@@ -89,13 +111,13 @@ public class TestingEndpoints
     @McpResource(name = "example1", uri = "file://example1.txt", description = "This is example1 resource.", mimeType = "text/plain")
     public ResourceContents example1Resource()
     {
-        return new ResourceContents("foo2", "file://example1.txt", "text/plain", "This is the content of file://example1.txt");
+        return new ResourceContents("foo2", "file://example1.txt", "text/plain", example1Content);
     }
 
     @McpResource(name = "example2", uri = "file://example2.txt", description = "This is example2 resource.", mimeType = "text/plain")
     public ResourceContents example2Resource()
     {
-        return new ResourceContents("foo2", "file://example2.txt", "text/plain", "This is the content of file://example2.txt");
+        return new ResourceContents("foo2", "file://example2.txt", "text/plain", example2Content);
     }
 
     @McpResourceTemplate(name = "template", uriTemplate = "file://{id}.template", description = "This is a resource template", mimeType = "text/plain")
@@ -155,5 +177,63 @@ public class TestingEndpoints
     {
         requestContext.sendLog(LoggingLevel.DEBUG, "This is debug");
         requestContext.sendLog(LoggingLevel.ALERT, "This is alert");
+    }
+
+    public enum VersionType
+    {
+        SYSTEM,
+        RESOURCE,
+    }
+
+    @McpTool(name = "setVersion", description = "Update the version of a system list or resource")
+    public void incrementListOrResource(VersionType type, String name)
+    {
+        switch (type) {
+            case SYSTEM -> {
+                switch (name) {
+                    case "tools" -> {
+                        ToolEntry firstTool = tools.iterator().next();
+                        Tool alteredTool = new Tool(
+                                firstTool.tool().name(),
+                                Optional.of(UUID.randomUUID().toString()),
+                                firstTool.tool().inputSchema(),
+                                firstTool.tool().outputSchema(),
+                                firstTool.tool().annotations());
+                        mcpServer.addTool(alteredTool, firstTool.toolHandler());
+                    }
+
+                    case "prompts" -> {
+                        PromptEntry firstPrompt = prompts.iterator().next();
+                        Prompt alteredPrompt = new Prompt(
+                                firstPrompt.prompt().name(),
+                                Optional.of(UUID.randomUUID().toString()),
+                                firstPrompt.prompt().role(),
+                                firstPrompt.prompt().arguments());
+                        mcpServer.addPrompt(alteredPrompt, firstPrompt.promptHandler());
+                    }
+
+                    case "resources" -> {
+                        ResourceEntry firstResource = resources.iterator().next();
+                        Resource alteredResource = new Resource(
+                                firstResource.resource().name(),
+                                firstResource.resource().uri(),
+                                Optional.of(UUID.randomUUID().toString()),
+                                firstResource.resource().mimeType(),
+                                firstResource.resource().size(),
+                                firstResource.resource().annotations());
+                        mcpServer.addResource(alteredResource, firstResource.handler());
+                    }
+
+                    default -> throw new IllegalArgumentException("Unknown system session version name: " + name);
+                }
+            }
+            case RESOURCE -> {
+                switch (name) {
+                    case "example1" -> example1Content = "Updated content of file://example1.txt: " + UUID.randomUUID();
+                    case "example2" -> example2Content = "Updated content of file://example2.txt: " + UUID.randomUUID();
+                    default -> throw new IllegalArgumentException("Unknown resource session version name: " + name);
+                }
+            }
+        }
     }
 }
