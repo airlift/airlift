b5ad8b6c228288b858ae14094d992155d2e9321f
Jordan Zimmerman <jordan@jordanzimmerman.com>
2025-12-28 09:30:28 +0000

    Protocol updates for Implementation
    Some new (optional) fields as well as a set of server icons.

--- mcp/src/main/java/io/airlift/mcp/model/Implementation.java
+++ mcp/src/main/java/io/airlift/mcp/model/Implementation.java
@@ -1,12 +1,42 @@
 package io.airlift.mcp.model;
 
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.google.common.base.MoreObjects.firstNonNull;
 import static java.util.Objects.requireNonNull;
 
-public record Implementation(String name, String version)
+public record Implementation(String name, String version, Optional<String> description, Optional<String> title, Optional<String> websiteUrl, Optional<List<Icon>> icons)
 {
     public Implementation
     {
         requireNonNull(name, "name is null");
         requireNonNull(version, "version is null");
+        description = firstNonNull(description, Optional.empty());
+        title = firstNonNull(title, Optional.empty());
+        websiteUrl = firstNonNull(websiteUrl, Optional.empty());
+        icons = firstNonNull(icons, Optional.empty());
+    }
+
+    public Implementation(String name, String version)
+    {
+        this(name, version, Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty());
+    }
+
+    public Implementation withAdditionalIcons(List<Icon> additionalIcons)
+    {
+        ImmutableList.Builder<Icon> builder = ImmutableList.builder();
+        icons.ifPresent(builder::addAll);
+        builder.addAll(additionalIcons);
+        List<Icon> updatedIcons = builder.build();
+
+        return new Implementation(name, version, description, title, websiteUrl, updatedIcons.isEmpty() ? Optional.empty() : Optional.of(updatedIcons));
+    }
+
+    public Implementation simpleForm()
+    {
+        return new Implementation(name, version, Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty());
     }
 }
