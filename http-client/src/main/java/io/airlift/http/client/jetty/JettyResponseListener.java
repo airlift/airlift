39d416499bdc99258aff1c0e642f085e1fc7d208
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2026-03-13 11:39:46 +0100

    HttpClient should be responsible for closing Response InputStream

--- http-client/src/main/java/io/airlift/http/client/jetty/JettyResponseListener.java
+++ http-client/src/main/java/io/airlift/http/client/jetty/JettyResponseListener.java
@@ -4,6 +4,10 @@ import org.eclipse.jetty.client.Request;
 import org.eclipse.jetty.client.Result;
 import org.eclipse.jetty.client.RetainingResponseListener;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+
 import static java.util.Objects.requireNonNull;
 
 class JettyResponseListener<T, E extends Exception>
@@ -32,7 +36,12 @@ class JettyResponseListener<T, E extends Exception>
             future.failed(result.getFailure());
         }
         else {
-            future.completed(result.getResponse(), takeContentAsInputStream());
+            try (InputStream stream = takeContentAsInputStream()) {
+                future.completed(result.getResponse(), stream);
+            }
+            catch (IOException e) {
+                future.failed(new UncheckedIOException("Failed communicating with server: " + result.getRequest().getURI().toASCIIString(), e));
+            }
         }
     }
 }
