92700ba924b6eec693894d27405e9f5ba9dc5b80
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2026-03-17 00:15:36 +0100

    Pass explicit Content-Type to FileBodyGenerator

--- http-client/src/main/java/io/airlift/http/client/FileBodyGenerator.java
+++ http-client/src/main/java/io/airlift/http/client/FileBodyGenerator.java
@@ -1,21 +1,36 @@
 package io.airlift.http.client;
 
+import com.google.common.net.MediaType;
+
 import java.nio.file.Path;
 
+import static com.google.common.net.MediaType.OCTET_STREAM;
 import static java.util.Objects.requireNonNull;
 
 public final class FileBodyGenerator
         implements BodyGenerator
 {
     private final Path path;
+    private final MediaType contentType;
+
+    public FileBodyGenerator(Path path, MediaType contentType)
+    {
+        this.path = requireNonNull(path, "path is null");
+        this.contentType = requireNonNull(contentType, "contentType is null");
+    }
 
     public FileBodyGenerator(Path path)
     {
-        this.path = requireNonNull(path, "path is null");
+        this(path, OCTET_STREAM);
     }
 
     public Path getPath()
     {
         return path;
+    }
+
+    public MediaType getContentType()
+    {
+        return contentType;
     }
 }
