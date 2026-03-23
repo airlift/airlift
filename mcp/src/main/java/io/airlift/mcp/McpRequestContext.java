c4996a31b4513cbf94b9546d42a027d624136148
Jordan Zimmerman <jordan@jordanzimmerman.com>
2026-03-23 14:03:38 +0000

    Add version of Operations that does not depend on sessions
    Note: not all current features are available. Most features will
    become available as the protocol evolves

--- mcp/src/main/java/io/airlift/mcp/McpRequestContext.java
+++ mcp/src/main/java/io/airlift/mcp/McpRequestContext.java
@@ -5,7 +5,6 @@ import io.airlift.mcp.model.InitializeRequest.ClientCapabilities;
 import io.airlift.mcp.model.JsonRpcResponse;
 import io.airlift.mcp.model.LoggingLevel;
 import io.airlift.mcp.model.Root;
-import io.airlift.mcp.sessions.Session;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.time.Duration;
@@ -17,8 +16,6 @@ public interface McpRequestContext
 {
     HttpServletRequest request();
 
-    Session session();
-
     Authenticated<?> identity();
 
     void sendProgress(double progress, double total, String message);
@@ -27,31 +24,24 @@ public interface McpRequestContext
 
     void sendMessage(String method, Optional<Object> params);
 
+    ClientCapabilities clientCapabilities();
+
+    // NOTE - may not be supported depending on how you've configured MCP - see the README regarding Sessions and Storage
     default void sendLog(LoggingLevel level, String message)
     {
         sendLog(level, Optional.empty(), Optional.of(message));
     }
 
-    default void sendLog(LoggingLevel level, Optional<String> logger, Optional<Object> data)
-    {
-        // only implemented when sessions are configured
-
-        throw new UnsupportedOperationException();
-    }
-
-    default ClientCapabilities clientCapabilities()
-    {
-        // only implemented when sessions are configured
-
-        throw new UnsupportedOperationException();
-    }
+    // NOTE - may not be supported depending on how you've configured MCP - see the README regarding Sessions and Storage
+    void sendLog(LoggingLevel level, Optional<String> logger, Optional<Object> data);
 
     /**
-     * Sends a server-to-client request and waits for the response until given timeout.
+     * Sends a server-to-client request and waits for the response until given timeout. NOTE - may not be supported depending on how you've configured MCP - see the README regarding Sessions and Storage
      */
     <R> JsonRpcResponse<R> serverToClientRequest(String method, Object params, Class<R> responseType, Duration timeout, Duration pollInterval)
             throws InterruptedException, TimeoutException;
 
+    // NOTE - may not be supported depending on how you've configured MCP - see the README regarding Sessions and Storage
     List<Root> requestRoots(Duration timeout, Duration pollInterval)
             throws InterruptedException, TimeoutException;
 }
