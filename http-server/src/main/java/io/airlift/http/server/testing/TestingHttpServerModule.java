1b49441f7f4cc87225f66ab8cc6be0f4ff82ddd2
Dain Sundstrom <dain@iq80.com>
2026-05-15 12:17:55 -0700

    Support qualified testing HTTP servers

--- http-server/src/main/java/io/airlift/http/server/testing/TestingHttpServerModule.java
+++ http-server/src/main/java/io/airlift/http/server/testing/TestingHttpServerModule.java
@@ -18,25 +18,35 @@ import com.google.inject.Key;
 import com.google.inject.Scopes;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
 import io.airlift.discovery.client.AnnouncementHttpServerInfo;
+import io.airlift.http.server.AnnouncementHttpServerInfoProvider;
 import io.airlift.http.server.HttpServer;
 import io.airlift.http.server.HttpServer.ClientCertificate;
 import io.airlift.http.server.HttpServerConfig;
 import io.airlift.http.server.HttpServerInfo;
+import io.airlift.http.server.HttpServerInfoProvider;
 import io.airlift.http.server.HttpsConfig;
 import io.airlift.http.server.LocalAnnouncementHttpServerInfo;
 import io.airlift.http.server.ServerFeature;
+import jakarta.annotation.Nullable;
 import jakarta.servlet.Filter;
 
+import java.lang.annotation.Annotation;
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
 import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
 import static io.airlift.configuration.ConfigBinder.configBinder;
 import static io.airlift.http.server.HttpServerBinder.HttpResourceBinding;
+import static java.util.Objects.requireNonNull;
 
 public class TestingHttpServerModule
         extends AbstractConfigurationAwareModule
 {
     private final String name;
     private final int httpPort;
+    private final Optional<Class<? extends Annotation>> qualifier;
+    private final @Nullable String configPrefix;
 
     public TestingHttpServerModule(String name)
     {
@@ -45,35 +55,78 @@ public class TestingHttpServerModule
 
     public TestingHttpServerModule(String name, int httpPort)
     {
-        this.name = name;
+        this(name, httpPort, Optional.empty(), null);
+    }
+
+    public TestingHttpServerModule(String name, Class<? extends Annotation> qualifier, String configPrefix)
+    {
+        this(name, 0, qualifier, configPrefix);
+    }
+
+    public TestingHttpServerModule(String name, int httpPort, Class<? extends Annotation> qualifier, String configPrefix)
+    {
+        this(name, httpPort, Optional.of(requireNonNull(qualifier, "qualifier is null")), requireNonNull(configPrefix, "configPrefix is null"));
+    }
+
+    private TestingHttpServerModule(String name, int httpPort, Optional<Class<? extends Annotation>> qualifier, @Nullable String configPrefix)
+    {
+        this.name = requireNonNull(name, "name is null");
         this.httpPort = httpPort;
+        this.qualifier = requireNonNull(qualifier, "qualifier is null");
+        this.configPrefix = configPrefix;
+
+        if (this.qualifier.isPresent()) {
+            checkArgument(this.configPrefix != null, "qualifier is present but configPrefix is null");
+        }
     }
 
     @Override
     protected void setup(Binder binder)
     {
-        configBinder(binder).bindConfig(HttpServerConfig.class);
-        configBinder(binder).bindConfigDefaults(HttpServerConfig.class, config -> config
+        configBinder(binder).bindConfig(qualifiedKey(HttpServerConfig.class), HttpServerConfig.class, configPrefix);
+        configBinder(binder).bindConfigDefaults(qualifiedKey(HttpServerConfig.class), config -> config
                 .setHttpPort(httpPort));
 
-        binder.bind(String.class).annotatedWith(ForTestingServer.class).toInstance(name);
-        binder.bind(HttpServerInfo.class).in(Scopes.SINGLETON);
-        binder.bind(TestingHttpServer.class).in(Scopes.SINGLETON);
-        newOptionalBinder(binder, ClientCertificate.class).setDefault().toInstance(ClientCertificate.NONE);
-        binder.bind(HttpServer.class).to(Key.get(TestingHttpServer.class));
-        newSetBinder(binder, ServerFeature.class);
-        newSetBinder(binder, Filter.class);
-        newSetBinder(binder, HttpResourceBinding.class);
-        binder.bind(AnnouncementHttpServerInfo.class).to(LocalAnnouncementHttpServerInfo.class);
+        if (qualifier.isPresent()) {
+            binder.bind(qualifiedKey(HttpServerInfo.class))
+                    .toProvider(new HttpServerInfoProvider(qualifier))
+                    .in(Scopes.SINGLETON);
+            binder.bind(qualifiedKey(TestingHttpServer.class))
+                    .toProvider(new TestingHttpServerProvider(name, qualifier))
+                    .in(Scopes.SINGLETON);
+            binder.bind(qualifiedKey(HttpServer.class)).to(qualifiedKey(TestingHttpServer.class));
+            binder.bind(qualifiedKey(AnnouncementHttpServerInfo.class))
+                    .toProvider(new AnnouncementHttpServerInfoProvider(qualifier))
+                    .in(Scopes.SINGLETON);
+        }
+        else {
+            binder.bind(String.class).annotatedWith(ForTestingServer.class).toInstance(name);
+            binder.bind(HttpServerInfo.class).in(Scopes.SINGLETON);
+            binder.bind(TestingHttpServer.class).in(Scopes.SINGLETON);
+            binder.bind(HttpServer.class).to(Key.get(TestingHttpServer.class));
+            binder.bind(AnnouncementHttpServerInfo.class).to(LocalAnnouncementHttpServerInfo.class);
+        }
 
-        newOptionalBinder(binder, HttpsConfig.class);
-        if (buildConfigObject(HttpServerConfig.class).isHttpsEnabled()) {
-            configBinder(binder).bindConfig(HttpsConfig.class);
-            configBinder(binder).bindConfigDefaults(HttpsConfig.class, config -> {
+        newOptionalBinder(binder, qualifiedKey(ClientCertificate.class)).setDefault().toInstance(ClientCertificate.NONE);
+        newSetBinder(binder, qualifiedKey(ServerFeature.class));
+        newSetBinder(binder, qualifiedKey(Filter.class));
+        newSetBinder(binder, qualifiedKey(HttpResourceBinding.class));
+
+        newOptionalBinder(binder, qualifiedKey(HttpsConfig.class));
+        if (buildConfigObject(qualifiedKey(HttpServerConfig.class), HttpServerConfig.class, configPrefix).isHttpsEnabled()) {
+            configBinder(binder).bindConfig(qualifiedKey(HttpsConfig.class), HttpsConfig.class, configPrefix);
+            configBinder(binder).bindConfigDefaults(qualifiedKey(HttpsConfig.class), config -> {
                 if (httpPort == 0) {
                     config.setHttpsPort(0);
                 }
             });
         }
+    }
+
+    private <T> Key<T> qualifiedKey(Class<T> type)
+    {
+        return qualifier
+                .map(annotation -> Key.get(type, annotation))
+                .orElseGet(() -> Key.get(type));
     }
 }
---
+++ http-server/src/main/java/io/airlift/http/server/testing/TestingHttpServerProvider.java
@@ -0,0 +1,80 @@
+package io.airlift.http.server.testing;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Provider;
+import com.google.inject.TypeLiteral;
+import io.airlift.http.server.HttpServer.ClientCertificate;
+import io.airlift.http.server.HttpServerBinder.HttpResourceBinding;
+import io.airlift.http.server.HttpServerConfig;
+import io.airlift.http.server.HttpServerInfo;
+import io.airlift.http.server.HttpsConfig;
+import io.airlift.http.server.ServerFeature;
+import io.airlift.node.NodeInfo;
+import jakarta.servlet.Filter;
+import jakarta.servlet.Servlet;
+
+import java.lang.annotation.Annotation;
+import java.util.Optional;
+import java.util.Set;
+
+import static com.google.common.base.Throwables.throwIfUnchecked;
+import static java.util.Objects.requireNonNull;
+
+class TestingHttpServerProvider
+        implements Provider<TestingHttpServer>
+{
+    private final String name;
+    private final Optional<Class<? extends Annotation>> qualifier;
+
+    private Injector injector;
+
+    TestingHttpServerProvider(String name, Optional<Class<? extends Annotation>> qualifier)
+    {
+        this.name = requireNonNull(name, "name is null");
+        this.qualifier = requireNonNull(qualifier, "qualifier is null");
+    }
+
+    @Inject
+    public void setInjector(Injector injector)
+    {
+        this.injector = requireNonNull(injector, "injector is null");
+    }
+
+    @Override
+    public TestingHttpServer get()
+    {
+        try {
+            return new TestingHttpServer(
+                    name,
+                    injector.getInstance(qualifiedKey(HttpServerInfo.class)),
+                    injector.getInstance(NodeInfo.class),
+                    injector.getInstance(qualifiedKey(HttpServerConfig.class)),
+                    injector.getInstance(qualifiedKey(new TypeLiteral<Optional<HttpsConfig>>() {})),
+                    injector.getInstance(qualifiedKey(Servlet.class)),
+                    injector.getInstance(qualifiedKey(new TypeLiteral<Set<Filter>>() {})),
+                    injector.getInstance(qualifiedKey(new TypeLiteral<Set<HttpResourceBinding>>() {})),
+                    injector.getInstance(qualifiedKey(new TypeLiteral<Set<ServerFeature>>() {})),
+                    injector.getInstance(qualifiedKey(ClientCertificate.class)));
+        }
+        catch (Exception e) {
+            throwIfUnchecked(e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> Key<T> qualifiedKey(Class<T> type)
+    {
+        return qualifier
+                .map(annotation -> Key.get(type, annotation))
+                .orElseGet(() -> Key.get(type));
+    }
+
+    private <T> Key<T> qualifiedKey(TypeLiteral<T> type)
+    {
+        return qualifier
+                .map(annotation -> Key.get(type, annotation))
+                .orElseGet(() -> Key.get(type));
+    }
+}
