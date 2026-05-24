141c8fdb68a430b82aa6afa99d2886ba8b5a7e90
Jordan Zimmerman <jordan@jordanzimmerman.com>
2026-05-24 08:24:07 +0100

    Support new caching mechanism

--- mcp/src/main/java/io/airlift/mcp/model/ReadResourceResult.java
+++ mcp/src/main/java/io/airlift/mcp/model/ReadResourceResult.java
@@ -3,11 +3,29 @@ package io.airlift.mcp.model;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
 
-public record ReadResourceResult(List<ResourceContents> contents)
+import static java.util.Objects.requireNonNull;
+
+public record ReadResourceResult(List<ResourceContents> contents, OptionalInt ttlMs, Optional<CacheScope> cacheScope)
+        implements CacheableResult
 {
     public ReadResourceResult
     {
         contents = ImmutableList.copyOf(contents);
+        requireNonNull(ttlMs, "ttlMs is null");
+        requireNonNull(cacheScope, "cacheScope is null");
+    }
+
+    public ReadResourceResult(List<ResourceContents> contents)
+    {
+        this(contents, OptionalInt.empty(), Optional.empty());
+    }
+
+    @Override
+    public ReadResourceResult withCacheableResult(int ttlMs, CacheScope cacheScope)
+    {
+        return new ReadResourceResult(contents, OptionalInt.of(ttlMs), Optional.of(cacheScope));
     }
 }
