b669f43c8a9caa5447a972d9d6616e4dd6c31caf
Ashhar Hasan <hashhar_dev@outlook.com>
2026-06-16 17:06:24 +0530

    Generalize BaseJacksonProvider to support other Jackson formats
    Switch the base from concrete JsonFactoryBuilder / JsonMapper.Builder to
    TSFBuilder / MapperBuilder so subclasses can supply any Jackson factory
    and mapper builder (e.g. YAMLFactoryBuilder / YAMLMapper.Builder).
    
    Move the JSON-specific AUTO_CLOSE_JSON_CONTENT disable into the JSON
    providers since it has no effect on other formats.

--- json/src/main/java/io/airlift/json/ObjectMapperProvider.java
+++ json/src/main/java/io/airlift/json/ObjectMapperProvider.java
@@ -2,7 +2,9 @@ package io.airlift.json;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonFactoryBuilder;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 import static java.util.Objects.requireNonNull;
 
@@ -11,7 +13,7 @@ import static java.util.Objects.requireNonNull;
  */
 @Deprecated
 public class ObjectMapperProvider
-        extends BaseJacksonProvider<ObjectMapper, ObjectMapperProvider>
+        extends BaseJacksonProvider<ObjectMapper, JsonMapper.Builder, ObjectMapperProvider>
 {
     public ObjectMapperProvider()
     {
@@ -25,7 +27,12 @@ public class ObjectMapperProvider
 
     private ObjectMapperProvider(JsonFactoryBuilder jsonFactoryBuilder)
     {
-        super(jsonFactoryBuilder);
+        super(jsonFactoryBuilder, JsonMapper::builder);
+
+        // When serialization fails in the middle, it's better to return a truncated (invalid) JSON
+        // than something that could be interpreted as a valid (but incorrect) result.
+        // This is especially applicable to server endpoints that return JSON responses.
+        mapperBuilder().disable(JsonGenerator.Feature.AUTO_CLOSE_JSON_CONTENT);
     }
 
     @Override
