4b08d00da64042797e1d8d7b181b1bfe031fb735
Jordan Zimmerman <jordan@jordanzimmerman.com>
2026-08-12 07:54:49 +0100

    Have ListRequest implement Meta

--- mcp/src/main/java/io/airlift/mcp/model/ListRequest.java
+++ mcp/src/main/java/io/airlift/mcp/model/ListRequest.java
@@ -1,14 +1,22 @@
 package io.airlift.mcp.model;
 
+import java.util.Map;
 import java.util.Optional;
 
 import static java.util.Objects.requireNonNullElse;
 
-public record ListRequest(Optional<String> cursor)
-        implements PaginatedRequest
+public record ListRequest(Optional<String> cursor, Optional<Map<String, Object>> meta)
+        implements PaginatedRequest, Meta<ListRequest>
 {
     public ListRequest
     {
         cursor = requireNonNullElse(cursor, Optional.empty());
+        meta = requireNonNullElse(meta, Optional.empty());
+    }
+
+    @Override
+    public ListRequest withMeta(Map<String, Object> meta)
+    {
+        return new ListRequest(cursor, Optional.of(meta));
     }
 }
